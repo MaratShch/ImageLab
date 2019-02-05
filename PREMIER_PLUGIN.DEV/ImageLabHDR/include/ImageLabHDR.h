5dbcc493577af614478ebec5ad3ae226a48367e5
Marat Shchuchinsky <maratshch@gmail.com>
2019-02-06 01:19:34 +0200

    initial commit

--- PREMIER_PLUGIN.DEV/ImageLabHDR/include/ImageLabHDR.h
+++ PREMIER_PLUGIN.DEV/ImageLabHDR/include/ImageLabHDR.h
@@ -1,2 +1,9 @@
 ﻿#pragma once
 
+#include "PrSDKEffect.h"
+#include "PrSDKPixelFormat.h"
+#include "PrSDKPPixSuite.h"
+#include "PrSDKPixelFormatSuite.h"
+#include "PrSDKSequenceInfoSuite.h"
+#include "SDK_File.h"
+
