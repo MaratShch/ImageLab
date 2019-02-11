0dfa183d961b3962fc18b76b566c0f272e172d26
Marat Shchuchinsky <maratshch@gmail.com>
2019-02-11 22:49:24 +0200

    add CPUID simple project

--- PerformanceTest/Histogram16CreateTest/src/main.cpp
+++ PerformanceTest/Histogram16CreateTest/src/main.cpp
@@ -13,7 +13,7 @@
 #define RANDOM_DATA_LIMIT 1024
 
 typedef struct {
-LARGE_INTEGER totalIterClcks;
+//LARGE_INTEGER totalIterClcks;
 LARGE_INTEGER iterClcks[MAX_TEST_LOOP];
 } trivialStatistics;
 
@@ -34,23 +34,26 @@ __declspec(noinline) void allocateHistogramPull(int** pHistogram)
 	// create interleaved histogram buffer
 	pHistogram[0] = reinterpret_cast<int*>(malloc(65536 * sizeof(int) * (HIST_POOL_SIZE)));
 	// create final histogram buffer
-	pHistogram[0] = reinterpret_cast<int*>(malloc(65536 * sizeof(int)));
+	pHistogram[1] = reinterpret_cast<int*>(malloc(65536 * sizeof(int)));
 	return;
 }
 
 __declspec(noinline) void freeHistogramPull(int** pHistogram)
 {
-	for (int i = 0; i < 2; i++)
+	if (nullptr != pHistogram[0])
 	{
-		if (nullptr != pHistogram[i])
-		{
-			free(pHistogram[i]);
-			pHistogram[i] = nullptr;
-		}
+		free(pHistogram[0]);
+		pHistogram[0] = nullptr;
+	}
+	if (nullptr != pHistogram[1])
+	{
+		free(pHistogram[1]);
+		pHistogram[1] = nullptr;
 	}
 
 	return;
 }
+
 
 __declspec(noinline) void createTrivialHistogram(const unsigned short* __restrict pData, const int sizeX, const int sizeY, const int linePitch, int* __restrict pHistogram)
 {
@@ -71,9 +74,126 @@ __declspec(noinline) void createTrivialHistogram(const unsigned short* __restric
 // memory optimized histogram computation
 __declspec(noinline) void createOptimize1dHistogram(const unsigned short* pData, const int sizeX, const int sizeY, const int linePitch, int** pHistogram)
 {
-	unsigned short* __restrict pImage = const_cast<unsigned short*>(pData);
-	int* __restrict pHistogramInterleaved = pHistogram  [0];
-	int* __restrict pHistogramFinal = pHistogram  [1];
+#if 0
+	const unsigned short* __restrict pImage = pData;
+	int* __restrict pHistogramInterleaved = pHistogram[0];
+	int* __restrict pHistogramFinal = pHistogram[1];
+
+	int* __restrict hist0 = &pHistogramInterleaved[0];
+	int* __restrict hist1 = &pHistogramInterleaved[1];
+	int* __restrict hist2 = &pHistogramInterleaved[2];
+	int* __restrict hist3 = &pHistogramInterleaved[3];
+
+
+	int    i, j;
+	unsigned int    ha0o, bn0e, hs0e;
+	unsigned int    ha0e, bn0o, hs0o;
+	unsigned int    ha1o, bn1e, hs1e;
+	unsigned int    ha1e, bn1o, hs1o;
+	unsigned int    ha2o, bn2e, hs2e;
+	unsigned int    ha2e, bn2o, hs2o;
+	unsigned int    ha3o, bn3e, hs3e;
+	unsigned int    ha3e, bn3o, hs3o;
+
+	/* ---------------------------------------------------------------- */
+	/*  Seed 'previous odd pixel' with bogus bin values that won't      */
+	/*  match the first even pixels.                                    */
+	/* ---------------------------------------------------------------- */
+
+	bn3o = 1;
+	bn2o = 1;
+	bn1o = 1;
+	bn0o = 1;
+
+	/* ---------------------------------------------------------------- */
+	/*  Prefetch the initial bins for each of our four histograms.      */
+	/*  This is needed for properly handling our forwarding logic.      */
+	/* ---------------------------------------------------------------- */
+	ha3o = hist3[bn3o];    /* read previous odd data */
+	ha2o = hist2[bn2o];    /* read previous odd data */
+	ha1o = hist1[bn1o];    /* read previous odd data */
+	ha0o = hist0[bn0o];    /* read previous odd data */
+
+						   /* make interleaved histogram */
+	for (j = 0; j < sizeY; j++)
+	{
+		const unsigned short* __restrict pLine = (unsigned short*)((__int64)(pData) + j * linePitch * sizeof(unsigned short));
+
+		/* make interleaved histogram from single line */
+		for (i = 0; i < sizeX; i += 8)
+		{
+			/*  Load 4 pixels from the even side of the image. */
+			bn0e = pLine[0];
+			bn1e = pLine[1];
+			bn2e = pLine[2];
+			bn3e = pLine[3];
+
+			hs3e = hist3[bn3e];      /* Get even bin.                */
+			hs2e = hist2[bn2e];      /* Get even bin.                */
+			hs1e = hist1[bn1e];      /* Get even bin.                */
+			hs0e = hist0[bn0e];      /* Get even bin.                */
+
+			hist3[bn3o] = ha3o;      /* Save previous odd bin.       */
+			hist2[bn2o] = ha2o;      /* Save previous odd bin.       */
+			hist1[bn1o] = ha1o;      /* Save previous odd bin.       */
+			hist0[bn0o] = ha0o;      /* Save previous odd bin.       */
+
+			ha3e = 1 + hs3e;         /* Update even bin.             */
+			ha2e = 1 + hs2e;         /* Update even bin.             */
+			ha1e = 1 + hs1e;         /* Update even bin.             */
+			ha0e = 1 + hs0e;         /* Update even bin.             */
+
+			ha3e += (bn3e == bn3o);   /* Add forwarding.              */
+			ha2e += (bn2e == bn2o);   /* Add forwarding.              */
+			ha1e += (bn1e == bn1o);   /* Add forwarding.              */
+			ha0e += (bn0e == bn0o);   /* Add forwarding.              */
+
+		  /* ------------------------------------------------------------ */
+		  /*  Load 4 pixels from the odd side of the image.               */
+		  /* ------------------------------------------------------------ */
+			bn0o = pLine[4];
+			bn1o = pLine[5];
+			bn2o = pLine[6];
+			bn3o = pLine[7];
+
+			hs3o = hist3[bn3o];      /* Get odd bin.                 */
+			hs2o = hist2[bn2o];      /* Get odd bin.                 */
+			hs1o = hist1[bn1o];      /* Get odd bin.                 */
+			hs0o = hist0[bn0o];      /* Get odd bin.                 */
+
+			hist3[bn3e] = ha3e;      /* Save previous even bin.      */
+			hist2[bn2e] = ha2e;      /* Save previous even bin.      */
+			hist1[bn1e] = ha1e;      /* Save previous even bin.      */
+			hist0[bn0e] = ha0e;      /* Save previous even bin.      */
+
+			ha3o = 1 + hs3o;         /* Update odd bin.              */
+			ha2o = 1 + hs2o;         /* Update odd bin.              */
+			ha1o = 1 + hs1o;         /* Update odd bin.              */
+			ha0o = 1 + hs0o;         /* Update odd bin.              */
+
+			ha3o += (bn3o == bn3e);   /* Add forwarding.              */
+			ha2o += (bn2o == bn2e);   /* Add forwarding.              */
+			ha1o += (bn1o == bn1e);   /* Add forwarding.              */
+			ha0o += (bn0o == bn0e);   /* Add forwarding.              */
+
+		}
+	}
+
+	/* ---------------------------------------------------------------- */
+	/*  Store final odd-pixel bin values.                               */
+	/* ---------------------------------------------------------------- */
+	hist3[bn3o] = ha3o;
+	hist2[bn2o] = ha2o;
+	hist1[bn1o] = ha1o;
+	hist0[bn0o] = ha0o;
+
+
+	/* histogram finalization - make final histogram from interleaved subs...*/
+	for (j = 0; j < 65536; j++)
+	{
+		pHistogramFinal[j] = hist0[j] + hist1[j] + hist2[j] + hist3[j];
+	}
+#endif
 
 	return;
 }
