1fe635875b0c88a3c64ea47f089660f94dfa5322
Marat Shchuchinsky <maratshch@gmail.com>
2019-07-05 21:42:13 +0300

    add plugin for Advanced White Balance (temporary empty)

---
+++ PREMIER_PLUGIN.DEV/ImageLabAWB/src/ImageLabAWB.cpp
@@ -0,0 +1,50 @@
+﻿#include "AdobeImageLabAWB.h"
+
+
+
+// ImageLabHDR filter entry point
+PREMPLUGENTRY DllExport xFilter(short selector, VideoHandle theData)
+{
+	csSDK_int32 errCode = fsNoErr;
+
+
+	switch (selector)
+	{
+		case fsInitSpec:
+			errCode = fsNoErr;
+
+		break;
+
+		case fsHasSetupDialog:
+			errCode = fsHasNoSetupDialog;
+		break;
+
+		case fsSetup:
+		break;
+
+		case fsExecute:
+		break;
+
+		case fsDisposeData:
+		break;
+
+		case fsCanHandlePAR:
+			errCode = prEffectCanHandlePAR;
+		break;
+			
+		case fsGetPixelFormatsSupported:
+			errCode = imageLabPixelFormatSupported(theData);
+		break;
+
+		case fsCacheOnLoad:
+			errCode = fsDoNotCacheOnLoad;
+		break;
+
+		default:
+			// unhandled case
+		break;
+		
+	}
+
+	return errCode;
+}
