ee0c2a51a971fc2e39337e50fd4e747150529ad0
Marat Shchuchinsky <maratshch@gmail.com>
2019-11-09 15:46:01 +0200

    add empty project for Bilateral Filter (Luma channel only) noise reduction

---
+++ PREMIER_PLUGIN.DEV/ImageLabBilateral/src/ImageLabPixelFormat.cpp
@@ -0,0 +1,26 @@
+#include "ImageLabBilateral.h"
+
+csSDK_int32 imageLabPixelFormatSupported (const VideoHandle theData)
+{
+	csSDK_int32 pixFormatResult = imNoErr;
+
+	if (nullptr != theData)
+	{
+		switch ((*theData)->pixelFormatIndex)
+		{
+			case 0:
+				(*theData)->pixelFormatSupported = PrPixelFormat_BGRA_4444_8u;
+			break;
+
+			default:
+				pixFormatResult = fsBadFormatIndex;
+			break;
+		}
+	}
+	else
+	{
+		pixFormatResult = imMemErr;
+	}
+
+	return pixFormatResult;
+}
