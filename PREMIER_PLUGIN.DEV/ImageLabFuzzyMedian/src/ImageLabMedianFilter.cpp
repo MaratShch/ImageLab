5ed62cf49c5bcf22ea0d6a5eebf0c622bdfe355f
Marat Shchuchinsky <maratshch@gmail.com>
2020-01-10 01:41:17 +0200

    add missed source files for PR Median Filter and AE ColorizeMe filter

---
+++ PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/src/ImageLabMedianFilter.cpp
@@ -0,0 +1,86 @@
+#include "ImageLabFuzzyMedian.h"
+
+bool median_filter_BGRA_4444_8u_frame (const VideoHandle theData, const csSDK_int32& kernelWidth)
+{
+#if !defined __INTEL_COMPILER 
+	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+	_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+#endif
+	
+	// protection on kernel size
+	if (MaxKernelWidth < kernelWidth || 3 > kernelWidth)
+		return false;
+
+	CACHE_ALIGN uint32_t windowR[MaxKernelElemSize];
+	CACHE_ALIGN uint32_t windowG[MaxKernelElemSize];
+	CACHE_ALIGN uint32_t windowB[MaxKernelElemSize];
+
+	prRect box = { 0 };
+
+	// Get the frame dimensions
+	((*theData)->piSuites->ppixFuncs->ppixGetBounds)((*theData)->destination, &box);
+
+	// Calculate dimensions
+	const csSDK_int32 height = box.bottom - box.top;
+	const csSDK_int32 width = box.right - box.left;
+	const csSDK_int32 rowbytes = ((*theData)->piSuites->ppixFuncs->ppixGetRowbytes)((*theData)->destination);
+	const csSDK_int32 linePitch = rowbytes >> 2;
+
+	// Create copies of pointer to the source, destination frames
+	const csSDK_uint32* __restrict srcPix = reinterpret_cast<csSDK_uint32* __restrict>(((*theData)->piSuites->ppixFuncs->ppixGetPixels)((*theData)->source));
+	      csSDK_uint32* __restrict dstPix = reinterpret_cast<csSDK_uint32* __restrict>(((*theData)->piSuites->ppixFuncs->ppixGetPixels)((*theData)->destination));
+
+	const csSDK_int32 border = kernelWidth >> 1;
+	const csSDK_int32 medianPosition = (kernelWidth * kernelWidth) / 2 + 1;
+	const csSDK_int32 lastLine = height - border;
+	const csSDK_int32 lastPixel = width - border;
+
+	csSDK_int32 idxPix = 0;
+	csSDK_int32 i, j, k, l, m;
+
+	for (j = border; j < lastLine; j++)
+	{
+		for (i = border; i < lastPixel; i++)
+		{
+			// Pick up window elements
+			const csSDK_int32 lMin = j - border;
+			const csSDK_int32 lMax = j + border;
+			const csSDK_int32 mMin = i - border;
+			const csSDK_int32 mMax = i + border;
+
+			k = 0;
+			for (l = lMin; l <= lMax; l++)
+			{
+				for (m = mMin; m <= mMax; m++)
+				{
+					const csSDK_uint32 inPixel = srcPix[l * linePitch + m];
+
+					windowB[k] = (inPixel & 0x000000FFu);
+					windowG[k] = (inPixel & 0x0000FF00u) >> 8;
+					windowR[k] = (inPixel & 0x00FF0000u) >> 16;
+
+					k++;
+				} /* for (m = i - border; m = i + border; i++) */
+			} /* for (l = j - border; l < j + border; l++) */
+
+		  // Order elements (only half of them)
+          __VECTOR_ALIGNED__
+          gnomesort(windowB, windowB + k);
+		  __VECTOR_ALIGNED__
+          gnomesort(windowG, windowG + k);
+		  __VECTOR_ALIGNED__
+		  gnomesort(windowR, windowR + k);
+
+		  idxPix = (j * linePitch + i);
+
+		  dstPix[idxPix] = (srcPix[idxPix] & 0xFF000000u) | /* copy ALPHA channel value as is */
+							windowB[medianPosition]       |
+						   (windowG[medianPosition] << 8) |
+						   (windowR[medianPosition] << 16);
+
+		} /* for (i = border; i < lastPixel; i++) */
+	
+	} /* for (j = border; j < lastLine; j++) */
+
+	return true;
+}
