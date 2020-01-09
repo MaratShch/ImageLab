3d3397f6aacfea9e9d27ec037d62dfdcedf91a8c
Marat Shchuchinsky <maratshch@gmail.com>
2020-01-10 01:38:10 +0200

    add first implementation of Median Filter for PR plugins for fomrat BGRA_8u [bordered pixels processing not implemented yet]

--- PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/include/ImageLabFuzzyMedian.h
+++ PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/include/ImageLabFuzzyMedian.h
@@ -19,17 +19,46 @@
 #define __VECTOR_ALIGNED__
 #endif
 
-
 template<typename T>
 T MIN(T a, T b) { return ((a < b) ? a : b); }
 
 template<typename T>
 T MAX(T a, T b) { return ((a > b) ? a : b); }
 
-template<typename T>
-T EXP(T val) {
-	return powf(Exp, val); // powf for floating
+template <typename T>
+inline void swap(T& a, T& b)
+{
+	const T tmpVar = a;
+	a = b;
+	b = tmpVar;
 }
+
+template <typename T>
+inline void swapEx(T& a, T& b)
+{
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+}
+
+template <typename T>
+inline void gnomesort (T* l, T* r) {
+	T* i = l;
+	while (i < r) {
+		if (i == l || *(i - 1) <= *i) i++;
+		else swapEx(*(i - 1), *i), i--;
+	}
+}
+
+
+constexpr int MaxKernelWidth = 15;
+constexpr int MaxKernelElemSize = MaxKernelWidth * MaxKernelWidth;
+
+typedef struct filterParams
+{
+	char		checkbox;
+} filterParams, *filterParamsP, **filterParamsH;
+
 
 
 // Declare plug-in entry point with C linkage
@@ -45,3 +74,5 @@ PREMPLUGENTRY DllExport xFilter(short selector, VideoHandle theData);
 
 csSDK_int32 imageLabPixelFormatSupported(const VideoHandle theData);
 
+csSDK_int32 selectProcessFunction(const VideoHandle theData, const bool& advFlag = false);
+bool median_filter_BGRA_4444_8u_frame (const VideoHandle theData, const csSDK_int32& kernelWidth = 3);
--- PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/src/AdobeImageLabFuzzyMedian.cpp
+++ PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/src/AdobeImageLabFuzzyMedian.cpp
@@ -2,15 +2,127 @@
 #include <windows.h>
 
 
+csSDK_int32 selectProcessFunction(const VideoHandle theData, const bool& advFlag)
+{
+	static constexpr char* strPpixSuite = "Premiere PPix Suite";
+	SPBasicSuite*		   SPBasic = nullptr;
+	csSDK_int32 errCode = fsBadFormatIndex;
+	bool processSucceed = true;
+
+	// acquire Premier Suites
+	if (nullptr != (SPBasic = (*theData)->piSuites->utilFuncs->getSPBasicSuite()))
+	{
+		PrSDKPPixSuite*			PPixSuite = nullptr;
+		const SPErr err = SPBasic->AcquireSuite(strPpixSuite, 1, (const void**)&PPixSuite);
+
+		if (nullptr != PPixSuite && kSPNoError == err)
+		{
+			PrPixelFormat pixelFormat = PrPixelFormat_Invalid;
+			PPixSuite->GetPixelFormat((*theData)->source, &pixelFormat);
+
+			switch (pixelFormat)
+			{
+				// ============ native AP formats ============================= //
+			case PrPixelFormat_BGRA_4444_8u:
+				median_filter_BGRA_4444_8u_frame(theData, 3);
+				break;
+
+			case PrPixelFormat_VUYA_4444_8u:
+				break;
+
+			case PrPixelFormat_VUYA_4444_8u_709:
+				break;
+
+			case PrPixelFormat_BGRA_4444_16u:
+				break;
+
+			case PrPixelFormat_BGRA_4444_32f:
+				break;
+
+			case PrPixelFormat_VUYA_4444_32f:
+				break;
+
+			case PrPixelFormat_VUYA_4444_32f_709:
+				break;
+
+				// ============ native AE formats ============================= //
+			case PrPixelFormat_ARGB_4444_8u:
+				break;
+
+			case PrPixelFormat_ARGB_4444_16u:
+				break;
+
+			case PrPixelFormat_ARGB_4444_32f:
+				break;
+
+				// =========== miscellanous formats =========================== //
+			case PrPixelFormat_RGB_444_10u:
+				break;
+
+				// =========== Packed uncompressed formats ==================== //
+			case PrPixelFormat_YUYV_422_8u_601:
+				break;
+
+			case PrPixelFormat_YUYV_422_8u_709:
+				break;
+
+			case PrPixelFormat_UYVY_422_8u_601:
+				break;
+
+			case PrPixelFormat_UYVY_422_8u_709:
+				break;
+
+			case PrPixelFormat_UYVY_422_32f_601:
+				break;
+
+			case PrPixelFormat_UYVY_422_32f_709:
+				break;
+
+			case PrPixelFormat_V210_422_10u_601:
+				break;
+
+			case PrPixelFormat_V210_422_10u_709:
+				break;
+
+			default:
+				processSucceed = false;
+				break;
+			}
+
+			errCode = (true == processSucceed) ? fsNoErr : errCode;
+		}
+	}
+
+	return errCode;
+}
+
+
 // Bilateral-RGB filter entry point
 PREMPLUGENTRY DllExport xFilter(short selector, VideoHandle theData)
 {
+	filterParamsH	paramsH = nullptr;
 	csSDK_int32 errCode = fsNoErr;
 
 	switch (selector)
 	{
-		case fsInitSpec:
-		break;
+	case fsInitSpec:
+		if ((*theData)->specsHandle)
+		{
+			// In a filter that has a need for a more complex setup dialog
+			// you would present your platform specific user interface here,
+			// storing results in the specsHandle (which you've allocated).
+		}
+		else
+		{
+			paramsH = reinterpret_cast<filterParamsH>(((*theData)->piSuites->memFuncs->newHandle)(sizeof(filterParams)));
+
+			// Memory allocation failed, no need to continue
+			if (nullptr == paramsH)
+				break;
+
+			(*paramsH)->checkbox = 0;
+			(*theData)->specsHandle = reinterpret_cast<char**>(paramsH);
+		}
 
 		case fsHasSetupDialog:
 			errCode = fsHasNoSetupDialog;
@@ -20,6 +132,12 @@ PREMPLUGENTRY DllExport xFilter(short selector, VideoHandle theData)
 		break;
 
 		case fsExecute:
+		{
+			// Get the data from specsHandle
+			paramsH = (filterParamsH)(*theData)->specsHandle;
+			const bool advFlag = (nullptr != paramsH ? ((*paramsH)->checkbox ? true : false) : false);
+			errCode = selectProcessFunction (theData, advFlag);
+		}
 		break;
 
 		case fsDisposeData:
