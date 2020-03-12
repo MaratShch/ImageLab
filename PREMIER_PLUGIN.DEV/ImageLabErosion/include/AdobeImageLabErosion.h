98a7b88f392e42e82e7eceeb7465be63551d6974
Marat Shchuchinsky <maratshch@gmail.com>
2020-03-13 01:08:09 +0200

    implement PR plugin for morphological operation - erosion

--- PREMIER_PLUGIN.DEV/ImageLabErosion/include/AdobeImageLabErosion.h
+++ PREMIER_PLUGIN.DEV/ImageLabErosion/include/AdobeImageLabErosion.h
@@ -36,6 +36,8 @@ T MIN(T a, T b) { return ((a < b) ? a : b); }
 template<typename T>
 T MAX(T a, T b) { return ((a > b) ? a : b); }
 
+constexpr csSDK_int32 defaultWindowSize = 3;
+
 // Declare plug-in entry point with C linkage
 #ifdef __cplusplus
 extern "C" {
@@ -50,3 +52,82 @@ PREMPLUGENTRY DllExport xFilter (short selector, VideoHandle theData);
 csSDK_int32 imageLabPixelFormatSupported(const VideoHandle theData);
 csSDK_int32 selectProcessFunction(VideoHandle theData);
 
+bool erosion_BGRA_4444_8u
+(
+	const csSDK_uint32* __restrict srcPix,
+	csSDK_uint32* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_VUYA_4444_8u
+(
+	const csSDK_uint32* __restrict srcPix,
+	csSDK_uint32* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_BGRA_4444_16u
+(
+	const csSDK_uint32* __restrict srcPix,
+	csSDK_uint32* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_BGRA_4444_32f
+(
+	const float* __restrict srcPix,
+	float* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_VUYA_4444_32f
+(
+	const float* __restrict srcPix,
+	float* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_ARGB_4444_8u
+(
+	const csSDK_uint32* __restrict srcPix,
+	csSDK_uint32* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_ARGB_4444_16u
+(
+	const csSDK_uint32* __restrict srcPix,
+	csSDK_uint32* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
+
+bool erosion_ARGB_4444_32f
+(
+	const float* __restrict srcPix,
+	float* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const csSDK_int32& windowSize
+);
