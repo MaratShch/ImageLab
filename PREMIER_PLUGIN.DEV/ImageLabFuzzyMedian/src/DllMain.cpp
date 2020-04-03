8a1e8ba44ca9c04f67ea4c80eaa41ad10eff6322
Marat Shchuchinsky <maratshch@gmail.com>
2020-04-04 00:36:52 +0300

    continue to implement Fuzzy Median Algo (non working and non completed yet)

---
+++ PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/src/DllMain.cpp
@@ -0,0 +1,51 @@
+#include <windows.h>
+#include <mutex>
+#include <thread>
+#include "ImageLabFuzzyMedian.h"
+
+
+std::mutex algStorageMutex;
+static AlgMemStorage algStorage;
+
+
+AlgMemStorage& getAlgStorageStruct (void)
+{
+	std::lock_guard<std::mutex> guard(algStorageMutex);
+	return algStorage;
+}
+
+void setAlgStorageStruct (const AlgMemStorage& storage)
+{
+	std::lock_guard<std::mutex> guard(algStorageMutex);
+	algStorage = storage;
+	return;
+}
+
+
+BOOL APIENTRY DllMain(HMODULE /* hModule */, DWORD ul_reason_for_call, LPVOID /* lpReserved */)
+{
+	switch (ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+			memset(&algStorage, 0, sizeof(algStorage));
+			algStorage.strSizeOf = sizeof(algStorage);
+		break;
+
+		case DLL_THREAD_ATTACH:
+		break;
+
+		case DLL_THREAD_DETACH:
+		break;
+
+		case DLL_PROCESS_DETACH:
+			algMemStorageFree (algStorage);
+			memset(&algStorage, 0, sizeof(algStorage));
+		break;
+
+		default:
+		break;
+	}
+
+	return TRUE;
+}
+
