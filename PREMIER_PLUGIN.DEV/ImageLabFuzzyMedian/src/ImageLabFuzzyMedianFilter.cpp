0db05d1dc8f59b5383d22dd5710ea33d0076de28
Marat Shchuchinsky <maratshch@gmail.com>
2020-04-09 00:37:14 +0300

    .

--- PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/src/ImageLabFuzzyMedianFilter.cpp
+++ PREMIER_PLUGIN.DEV/ImageLabFuzzyMedian/src/ImageLabFuzzyMedianFilter.cpp
@@ -3,6 +3,12 @@
 #include <assert.h> 
 #include <float.h>
 
+#ifdef _DEBUG
+static float fDbgStd[2048] = {};
+static unsigned int dbgCnt = 0u;
+static float* pInput = nullptr;
+#endif
+
 
 /* fast SQUARE ROOT COMPUTATION */
 static inline float asqrt (const float& x)
@@ -14,7 +20,7 @@ static inline float asqrt (const float& x)
 	return xRes * x;
 }
 
-inline float get_matrix_std
+static inline float get_matrix_std
 (
 	const float* __restrict pBuffer,	/* buffer pointer								*/	
 	const csSDK_int32& winSize,			/* size of MATRIX'  window						*/
@@ -54,8 +60,9 @@ inline float get_matrix_std
 	}
 
 	variance = (fSum / static_cast<float>(winSqSize - 1));
-
-	return asqrt(variance);
+	/* use fast square root computation for STD */
+//	return asqrt(variance);
+	return sqrt(variance);
 }
 
 
@@ -63,7 +70,8 @@ float get_min_std
 (
 	float* __restrict	pBuffer,
 	const  csSDK_int32&	width,
-	const  csSDK_int32& height
+	const  csSDK_int32& height,
+	const  csSDK_int32& linePitch
 )
 {
 	constexpr csSDK_int32 winSize = 8;
@@ -74,20 +82,27 @@ float get_min_std
 	float fStdMin = FLT_MAX;
 
 	// test matrix STD: expected value 2.73 (from Matlab)
-	// const float fff[9] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f };
-	// fStd = get_matrix_std(fff, 3, 3);
-
-	const csSDK_int32 linePitch = width * 3;
+	// constexpr float H = 0.f, S = 0.f, V = 1.0f ... 9.0f;
+	// constexpr float fff[27] = { H, S, 1.0f, H, S, 2.0f, H, S, 3.0f, H, S, 4.0f, H, S, 5.0f, H, S, 6.0f, H, S, 7.0f, H, S, 8.0f, H, S, 9.0f };
+	// fStd = get_matrix_std(fff, 3, 9);
+	//
+#ifdef _DEBUG
+	dbgCnt = 0u;
+#endif
 
 	for (j = 0; j < heightMax; j += winSize)
 	{
+		const float* pLine = &pBuffer[j * linePitch];
+
 		for (i = 0; i < widthMax; i += winSize)
 		{
-			idx = j * linePitch + i * 3;
-			const float* __restrict pMatrix = &pBuffer[idx];
+			fStd = get_matrix_std(&pLine[i * 3], winSize, linePitch);
+#ifdef _DEBUG
+			fDbgStd[dbgCnt] = fStd;
+			dbgCnt++;
+#endif
+			fStdMin = ((0.0f == fStd) ? fStdMin : MIN(fStdMin, fStd));
 
-			fStd = get_matrix_std(pMatrix, winSize, linePitch);
-			fStdMin = ((0 == fStd) ? fStdMin : MIN(fStdMin, fStd));
 		} /* for (i = 0; i < widthMax; i += winSize) */
 
 	} /* for (j = 0; j < heightMax; j += winSize) */
@@ -103,19 +118,113 @@ void fuzzy_filter_median_3x3
 	const  csSDK_int32& height
 )
 {
-	/* get STD */
-	const float fImgStdMin = get_min_std(pBuffer, width, height);
-	const float p = 6 * fImgStdMin;
-	const csSDK_int32 maxPix = width - 1;
-	const csSDK_int32 maxLine = height - 1;
+	float* fPixel = nullptr;
 	csSDK_int32 i, j;
+	const csSDK_int32 maxPix  = width - 2;
+	const csSDK_int32 maxLine = height - 2;
+
+	csSDK_int32 idxNE, idxE, idxS, idxW;
+	csSDK_int32 idxK11, idxK12, idxK13, idxK21, idxK22, idxK23, idxK31, idxK32, idxK33;
+	float uu, dd, ll, rr, mm, corr, diff;
+	float a, b, c, d;
+	float dA, dB, dC;
+
+#ifdef _DEBUG
+	pInput = pBuffer;
+#endif
+
+	const csSDK_int32 linePitch = width * 3;
+
+	/* get STD */
+	const float fImgStdMin = get_min_std(pBuffer, width, height, linePitch);
+	const float p = 6 * fImgStdMin;
 
 	/* currently, lets check in-place processing for avoid additional memory allocation */
-	for (j = 1; j < maxLine; j++)
+	for (j = 2; j < maxLine; j++)
 	{
-		for (i = 1; i < maxLine; i++)
+		for (i = 2; i < maxPix; i++)
 		{
+			corr = 0.f;
 
+			/* index of Pixel [0, 0] inside of kernel */
+			idxK11 = OFFSET_V(j * linePitch + i * 3);
+			/* index of Pixel [0, 1] inside of kernel */
+			idxK12 = idxK11 + 3;
+			/* index of Pixel [0, 2] inside of kernel */
+			idxK13 = idxK11 + 6;
+			/* index of Pixel [1, 0] inside of kernel */
+			idxK21 = OFFSET_V((j + 1) * linePitch + i * 3);
+			/* index of Pixel [1, 1] inside of kernel */
+			idxK22 = idxK21 + 3;
+			/* index of Pixel [1, 2] inside of kernel */
+			idxK23 = idxK21 + 6;
+			/* index of Pixel [2, 0] insode of kernel */
+			idxK31 = OFFSET_V((j + 2) * linePitch + i * 3);
+			/* index of Pixel [2, 1] insode of kernel */
+			idxK32 = idxK31 + 3;
+			/* index of Pixel [2, 2] insode of kernel */
+			idxK33 = idxK31 + 6;
+
+
+			/* index of Norht-East pixel outside of kernel*/
+			idxNE = OFFSET_V((j - 2) * linePitch + (i - 2) * 3);
+			/* index of East pixel outside of kernel */
+			idxE  = OFFSET_V(j * linePitch + (i - 2) * 3);
+			/* index of South pixel outside of kernel */
+			idxS  = OFFSET_V((j + 2) * linePitch + i * 3);
+			/* index of West pixel outside of kernel */
+			idxW = OFFSET_V(j + linePitch + (i + 2) * 3);
+
+			uu  = pBuffer[idxNE];
+			ll  = pBuffer[idxW];
+			dd  = pBuffer[idxS];
+			rr  = pBuffer[idxE];
+
+			mm = (pBuffer[idxK11] + pBuffer[idxK12] + pBuffer[idxK13] + pBuffer[idxK21]) / 4.0f;
+
+		/*	
+				|
+				|->
+				|
+		*/
+			a = pBuffer[idxK12];
+			b = pBuffer[idxK22];
+			c = pBuffer[idxK23];
+
+			dA = a - pBuffer[idxK13];
+			dB = b - pBuffer[idxK23];
+			dC = c - pBuffer[idxK33];
+
+			d = (MIN(abs(dA), MIN(abs(dB), abs(dC)))) / p;
+			d = MIN(d, 1.f);
+			d = 1.0f - d;
+
+			diff = pBuffer[idxK23] - b;
+			corr = corr + d * diff;
+
+		/*
+				|
+			  <-|
+				|
+		*/
+			dA = a - pBuffer[idxK11];
+			dB = b - pBuffer[idxK21];
+			dC = c - pBuffer[idxK31];
+
+			d = (MIN(abs(dA), MIN(abs(dB), abs(dC)))) / p;
+			d = MIN(d, 1.f);
+			d = 1.0f - d;
+			
+			diff = pBuffer[idxK21] - b;
+			corr = corr + d * diff;
+
+		/*
+			     ^ 
+			     |
+			    ___
+
+		*/
+			
 		}
 
 	} /* for (j = 1; j < maxLine; j++) */
