876c9f7cbb91f6076f07d6cefb4b018caf8d6730
Marat Shchuchinsky <maratshch@gmail.com>
2020-04-14 23:40:00 +0300

    Start implement Video Mosaic plugin for Adobe PM

---
+++ PREMIER_PLUGIN.DEV/ImageLabVideoMosaic/include/ImageLabVideoMosaic.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "PrSDKEffect.h"
+#include "PrSDKPixelFormat.h"
+#include "PrSDKPPixSuite.h"
+#include "PrSDKPixelFormatSuite.h"
+#include "PrSDKSequenceInfoSuite.h"
+#include "SDK_File.h"
+
+
+#define CACHE_LINE		64
+#define CPU_PAGE_SIZE	4096
+
+#define CACHE_ALIGN __declspec(align(CACHE_LINE))
+
+#define AVX2_ALIGN __declspec(align(32))
+#define AVX512_ALIGN __declspec(align(64))
+
+#if defined __INTEL_COMPILER 
+#define __VECTOR_ALIGNED__ __pragma(vector aligned)
+#define __ASSUME_ALIGNED(a, align_val) __assume_aligned(a, align_val)
+#else
+#define __VECTOR_ALIGNED__
+#define __ASSUME_ALIGNED__         
+#endif
+
+constexpr csSDK_int16 minBlocksNumber = 2;				/* minimal blocks number per one dimension				*/
+constexpr csSDK_int16 maxBlocksNumber = 16;				/* maximal blocks number per one dimension				*/
+constexpr csSDK_int16 defBlocksNumber = 4;				/* maximal blocks number per one dimension				*/
+constexpr csSDK_int16 minMosaicMapDuration = 20;		/* minimal number of frames for use current mosaic map	*/
+constexpr csSDK_int16 defMosaicMapDuration = 200;		/* default number of frames for use current mosaic map	*/
+constexpr csSDK_int16 maxMosaicMapDuration = SHRT_MAX;	/* maximal number of frame for use current mosaic map	*/
+constexpr csSDK_int16 infiniteMosaicMapDuration = -1;
+
+constexpr csSDK_int16 maxMosaicMapSize = maxBlocksNumber * maxBlocksNumber;
+constexpr csSDK_int16 lineIdx = 0;
+constexpr csSDK_int16 rowIdx  = 1;
+
+typedef union mosaicMap
+{
+	csSDK_int32 mapElem;
+	csSDK_int16 mapIdx[2];
+}mosaicMap;
+
+typedef struct filterParams
+{
+	// filter setting
+	csSDK_int16	sliderBlocksNumber;
+	csSDK_int16	sliderFrameDuration;
+	csSDK_int32 frameCnt;
+	mosaicMap   map[maxMosaicMapSize];
+} filterParams, *filterParamsP, **filterParamsH;
+
+
+
+
+// Declare plug-in entry point with C linkage
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+PREMPLUGENTRY DllExport xFilter (short selector, VideoHandle theData);
+
+#ifdef __cplusplus
+}
+#endif
+
+csSDK_int32 imageLabPixelFormatSupported(const VideoHandle theData);
+bool make_mosaic_map (mosaicMap* pMap, const csSDK_int16 blocksNumber);
+
+template <typename T>
+bool make_mosaic_image
+(
+	const T* __restrict srcPix,
+	T* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const mosaicMap* pMosaic = nullptr
+);
---
+++ PREMIER_PLUGIN.DEV/ImageLabVideoMosaic/src/ImageLabVideoMosaic.cpp
@@ -0,0 +1,220 @@
+﻿#include "ImageLabVideoMosaic.h"
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+
+bool make_mosaic_map (mosaicMap* pMap, const csSDK_int16 blocksNumber)
+{
+	if (nullptr == pMap || blocksNumber < minBlocksNumber || blocksNumber > maxBlocksNumber)
+		return false;
+
+	std::vector<csSDK_int16> blockVertical;
+	std::vector<csSDK_int16> blockHorizontal;
+	csSDK_int16 i;
+
+	/* build vector */
+	for (i = 0; i < blocksNumber; i++)
+	{
+		blockVertical.push_back(i);
+		blockHorizontal.push_back(i);
+	} /* for (i = 0; i < blocksNumber; i++) */
+
+	/* initialize random device */
+	std::random_device rd;
+	std::mt19937 g(rd());
+
+	/* shuffle numbers */
+	std::shuffle(blockVertical.begin(),   blockVertical.end(),   g);
+	std::shuffle(blockHorizontal.begin(), blockHorizontal.end(), g);
+
+	for (i = 0; i < blocksNumber; i++)
+	{
+		pMap[i].mapIdx[lineIdx] = blockHorizontal[i];
+		pMap[i].mapIdx[rowIdx]  = blockVertical[i];
+	} /* for (i = 0; i < blocksNumber; i++) */
+
+	return true;
+}
+
+
+template <typename T>
+bool make_mosaic_image
+(
+	const T* __restrict srcPix,
+	T* __restrict dstPix,
+	const csSDK_int32& width,
+	const csSDK_int32& height,
+	const csSDK_int32& linePitch,
+	const mosaicMap* pMosaic
+)
+{
+	return true;
+}
+
+
+csSDK_int32 selectProcessFunction (const VideoHandle theData)
+{
+	static constexpr char* strPpixSuite = "Premiere PPix Suite";
+	SPBasicSuite*		   SPBasic = nullptr;
+	filterParamsH		   paramsH = nullptr;
+	constexpr long         siteVersion = 1l;
+	csSDK_int32            errCode = fsBadFormatIndex;
+	bool                   processSucceed = true;
+
+	// acquire Premier Suites
+	if (nullptr != (SPBasic = (*theData)->piSuites->utilFuncs->getSPBasicSuite()))
+	{
+		PrSDKPPixSuite*	  PPixSuite = nullptr;
+		const SPErr err = SPBasic->AcquireSuite(strPpixSuite, siteVersion, (const void**)&PPixSuite);
+
+		if (nullptr != PPixSuite && kSPNoError == err)
+		{
+			PrPixelFormat pixelFormat = PrPixelFormat_Invalid;
+			PPixSuite->GetPixelFormat((*theData)->source, &pixelFormat);
+
+			// Get the frame dimensions
+			prRect box = {};
+			((*theData)->piSuites->ppixFuncs->ppixGetBounds)((*theData)->destination, &box);
+
+			// Calculate dimensions
+			const csSDK_int32 height = box.bottom - box.top;
+			const csSDK_int32 width = box.right - box.left;
+			const csSDK_int32 linePitch = (((*theData)->piSuites->ppixFuncs->ppixGetRowbytes)((*theData)->destination)) >> 2;
+
+			// Check is frame dimensions are correct
+			if (0 >= height || 0 >= width || 0 >= linePitch || linePitch < width)
+				return fsBadFormatIndex;
+
+			paramsH = reinterpret_cast<filterParamsH>((*theData)->specsHandle);
+			if (nullptr == paramsH)
+				return fsBadFormatIndex;
+
+			void* __restrict srcImg = reinterpret_cast<void* __restrict>(((*theData)->piSuites->ppixFuncs->ppixGetPixels)((*theData)->source));
+			void* __restrict dstImg = reinterpret_cast<void* __restrict>(((*theData)->piSuites->ppixFuncs->ppixGetPixels)((*theData)->destination));
+			if (nullptr == srcImg || nullptr == dstImg)
+				return fsBadFormatIndex;
+
+			(*paramsH)->frameCnt += 1;
+			if ((*paramsH)->frameCnt > (*paramsH)->sliderFrameDuration)
+			{
+				(*paramsH)->frameCnt = 0;
+				make_mosaic_map((*paramsH)->map, (*paramsH)->sliderBlocksNumber);
+			}
+
+			switch (pixelFormat)
+			{
+				// ============ native AP formats ============================= //
+				case PrPixelFormat_BGRA_4444_8u:
+				case PrPixelFormat_VUYA_4444_8u:
+				case PrPixelFormat_VUYA_4444_8u_709:
+				case PrPixelFormat_ARGB_4444_8u:
+				case PrPixelFormat_RGB_444_10u:
+				{
+					const csSDK_uint32* __restrict pSrcPix = reinterpret_cast<const csSDK_uint32* __restrict>(srcImg);
+					csSDK_uint32* __restrict pDstPix = reinterpret_cast<csSDK_uint32* __restrict>(dstImg);
+					processSucceed = make_mosaic_image (pSrcPix, pDstPix, width, height, linePitch, (*paramsH)->map);
+				}
+				break;
+
+				case PrPixelFormat_BGRA_4444_16u:
+				case PrPixelFormat_ARGB_4444_16u:
+				{
+					const csSDK_uint64* __restrict pSrcPix = reinterpret_cast<const csSDK_uint64* __restrict>(srcImg);
+					csSDK_uint64* __restrict pDstPix = reinterpret_cast<csSDK_uint64* __restrict>(dstImg);
+					processSucceed = make_mosaic_image (pSrcPix, pDstPix, width, height, linePitch, (*paramsH)->map);
+				}
+				break;
+
+				case PrPixelFormat_BGRA_4444_32f:
+				case PrPixelFormat_VUYA_4444_32f:
+				case PrPixelFormat_VUYA_4444_32f_709:
+				case PrPixelFormat_ARGB_4444_32f:
+				{
+					const __m128i* __restrict pSrcPix = reinterpret_cast<const __m128i* __restrict>(srcImg);
+					__m128i* __restrict pDstPix = reinterpret_cast<__m128i* __restrict>(dstImg);
+					processSucceed = make_mosaic_image (pSrcPix, pDstPix, width, height, linePitch, (*paramsH)->map);
+				}
+				break;
+
+				default:
+					processSucceed = false;
+				break;
+			}
+
+			SPBasic->ReleaseSuite (strPpixSuite, siteVersion);
+			errCode = (true == processSucceed) ? fsNoErr : errCode;
+		}
+	}
+
+	return errCode;
+}
+
+
+// ImageLabHDR filter entry point
+PREMPLUGENTRY DllExport xFilter(short selector, VideoHandle theData)
+{
+	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+	_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+
+	filterParamsH	paramsH = nullptr;
+	csSDK_int32		errCode = fsNoErr;
+
+	switch (selector)
+	{
+		case fsInitSpec:
+			if ((*theData)->specsHandle)
+			{
+				// In a filter that has a need for a more complex setup dialog
+				// you would present your platform specific user interface here,
+				// storing results in the specsHandle (which you've allocated).
+			}
+			else
+			{
+				paramsH = reinterpret_cast<filterParamsH>(((*theData)->piSuites->memFuncs->newHandle)(sizeof(filterParams)));
+
+				// Memory allocation failed, no need to continue
+				if (nullptr == paramsH)
+					break;
+
+				make_mosaic_map((*paramsH)->map, defBlocksNumber);
+				(*paramsH)->sliderBlocksNumber = defBlocksNumber;
+				(*paramsH)->sliderFrameDuration = defMosaicMapDuration;
+				(*paramsH)->frameCnt = 0;
+				(*theData)->specsHandle = reinterpret_cast<char**>(paramsH);
+			}
+			break;
+
+		case fsSetup:
+		break;
+
+		case fsExecute:
+			errCode = selectProcessFunction(theData);
+		break;
+
+		case fsDisposeData:
+			(*theData)->piSuites->memFuncs->disposeHandle((*theData)->specsHandle);
+			(*theData)->specsHandle = nullptr;
+		break;
+
+		case fsCanHandlePAR:
+			errCode = prEffectCanHandlePAR;
+		break;
+			
+		case fsGetPixelFormatsSupported:
+			errCode = imageLabPixelFormatSupported(theData);
+		break;
+
+		case fsCacheOnLoad:
+			errCode = fsDoNotCacheOnLoad;
+		break;
+
+		default:
+			// unhandled case
+		break;
+		
+	}
+
+	return errCode;
+}
