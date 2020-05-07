5decc75091558eb71b3fa6bdc00463bd9846dd63
Marat Shchuchinsky <maratshch@gmail.com>
2020-05-07 23:11:27 +0300

    add coomon Adobe AE includes

---
+++ AFTEREFFECT_PLUGIN.DEV/Common/CommonAdobeAE.hpp
@@ -0,0 +1,19 @@
+#ifndef __IMAGE_LAB2_ADOBE_AE_COMMON_INCLUDES_FILES__
+#define __IMAGE_LAB2_ADOBE_AE_COMMON_INCLUDES_FILES__
+
+#include "AEConfig.h"
+#include "entry.h"
+#ifdef AE_OS_WIN
+#include "string.h"
+#endif
+#include "AE_Effect.h"
+#include "AE_EffectCB.h"
+#include "AE_Macros.h"
+#include "AE_EffectCBSuites.h"
+#include "AE_GeneralPlug.h"
+#include "AEFX_SuiteHandlerTemplate.h"
+#include "PrSDKAESupport.h"
+#include "Common.hpp"
+#include "Param_Utils.h"
+
+#endif /* __IMAGE_LAB2_ADOBE_AE_COMMON_INCLUDES_FILES__ */
