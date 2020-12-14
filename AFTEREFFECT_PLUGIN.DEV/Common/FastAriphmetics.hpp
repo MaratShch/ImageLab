bbd61a979f9df6bc34abaa06600d239d0a114548
Marat Shchuchinsky <maratshch@gmail.com>
2020-12-14 23:34:33 +0200

    add fast ariphmetic header

--- AFTEREFFECT_PLUGIN.DEV/Common/FastAriphmetics.hpp
+++ AFTEREFFECT_PLUGIN.DEV/Common/FastAriphmetics.hpp
@@ -1,20 +1,36 @@
 #pragma once
 
-inline double asqrt(const double& x)
+namespace FastCompute
 {
-	const double   xHalf = 0.50 * x;
-	long long int  tmp = 0x5FE6EB50C7B537AAl - (*(long long int*)&x >> 1); //initial guess
-	double         xRes = *(double*)&tmp;
-	xRes *= (1.50 - (xHalf * xRes * xRes));
-	return xRes * x;
-}
+	template <typename T>
+	inline constexpr typename std::enable_if<std::is_integral<T>::value, T>::type Min(const T& x, const T& y)
+	{	/* find minimal value between 2 fixed point values without branch */
+		return y ^ ((x ^ y) & -(x < y));
+	}
 
-inline float asqrt(const float& x)
-{
-	const float xHalf = 0.50f * x;
-	int   tmp = 0x5F3759DF - (*(int*)&x >> 1); //initial guess
-	float xRes = *(float*)&tmp;
-	xRes *= (1.50f - (xHalf * xRes * xRes));
-	return xRes * x;
-}
+	template <typename T>
+	inline constexpr typename std::enable_if<std::is_integral<T>::value, T>::type Max(const T& x, const T& y)
+	{   /* find maximal value between 2 fixed point values without branch */
+		return x ^ ((x ^ y) & -(x < y));
+	}
 
+
+	inline double asqrt(const double x)
+	{
+		const double   xHalf = 0.50 * x;
+		long long int  tmp = 0x5FE6EB50C7B537AAl - (*(long long int*)&x >> 1); //initial guess
+		double         xRes = *(double*)&tmp;
+		xRes *= (1.50 - (xHalf * xRes * xRes));
+		return xRes * x;
+	}
+
+	inline float asqrt(const float x)
+	{
+		const float xHalf = 0.50f * x;
+		int   tmp = 0x5F3759DF - (*(int*)&x >> 1); //initial guess
+		float xRes = *(float*)&tmp;
+		xRes *= (1.50f - (xHalf * xRes * xRes));
+		return xRes * x;
+	}
+
+}; /* namespace FastCompute */
