eeee12274a65979bcc6867ac9136d90d4b992f32
Marat Shchuchinsky <maratshch@gmail.com>
2021-01-18 22:44:16 +0200

    QT5.15.2 build for Windows 10 with VS-2015

--- qt-build.bat
+++ qt-build.bat
@@ -1,5 +1,6 @@
 REM https://stackoverflow.com/questions/14932315/how-to-compile-qt-5-under-windows-or-linux-32-or-64-bit-static-or-dynamic-on-v
-
+REM QT sources: http://download.qt.io/official_releases/
+ 
 cls
 
 set QTDIR= 
@@ -13,6 +14,6 @@ set PATH=%QTDIR%\bin;%PATH%
 mkdir qt5_build
 cd qt5_build
 
-..\5.15.2\configure -debug-and-release -opensource -platform win32-msvc -qt-zlib -qt-libpng -qt-libjpeg -opengl desktop
+..\5.15.2\configure -verbose -debug-and-release -opensource -confirm-license -platform win32-msvc -avx2 -qt3d-simd avx2 -feature-vulkan -qt-zlib -qt-libpng -qt-libjpeg -opengl dynamic -I "C:\VulkanSDK\1.2.162.1\Include" -I "C:\Autodesk\FBX_SDK\2020.1\include" -L "C:\VulkanSDK\1.2.162.1\Lib" -L "C:\Autodesk\FBX_SDK\2020.1\lib\vs2015\x64\release" -D "C:\VulkanSDK\1.2.162.1\Bin" -nomake examples -nomake tests
 
 ..\jom\jom -j 8
