0e93282b30729ecfaff64c0fcf3b3febeb2ef697
Marat Shchuchinsky <maratshch@gmail.com>
2021-01-22 01:27:22 +0200

    add class for apply to GPU

---
+++ AFTEREFFECT_PLUGIN.DEV/CommonGPULib/include/ImageLab2GpuObj.hpp
@@ -0,0 +1,200 @@
+#pragma once
+
+#include "PrSDKGPUDeviceSuite.h"
+#include "PrSDKGPUImageProcessingSuite.h"
+#include "PrSDKGPUFilter.h"
+#include "PrSDKMemoryManagerSuite.h"
+#include "PrSDKPPixSuite.h"
+#include "PrSDKPPix2Suite.h"
+#include "PrSDKVideoSegmentSuite.h"
+#include "ClassRestrictions.hpp"
+#include <atomic>
+
+class CImageLab2GpuObj
+{
+public:
+
+	CLASS_NON_COPYABLE(CImageLab2GpuObj);
+	CLASS_NON_MOVABLE(CImageLab2GpuObj);
+
+	CImageLab2GpuObj();
+	virtual ~CImageLab2GpuObj();
+
+	static prSuiteError Startup (piSuitesPtr piSuites, csSDK_int32 inIndex)
+	{
+		return suiteError_NoError;
+	}
+
+	static prSuiteError Shutdown (piSuitesPtr piSuites, csSDK_int32 inIndex)
+	{
+		return suiteError_NoError;
+	}
+
+	static csSDK_int32 PluginCount()
+	{
+		return 1;
+	}
+
+	static PrSDKString MatchName (piSuitesPtr piSuites, csSDK_int32 inIndex)
+	{
+		return PrSDKString();
+	}
+
+	virtual prSuiteError Initialize(PrGPUFilterInstance* ioInstanceData);
+	virtual prSuiteError Cleanup (void);
+
+	virtual prSuiteError GetFrameDependencies(const PrGPUFilterRenderParams* inRenderParams, csSDK_int32* ioQueryIndex, PrGPUFilterFrameDependency* outFrameRequirements);
+
+	virtual prSuiteError Precompute(const PrGPUFilterRenderParams* inRenderParams, csSDK_int32 inIndex, PPixHand inFrame);
+
+	virtual prSuiteError Render(const PrGPUFilterRenderParams* inRenderParams, const PPixHand* inFrames, csSDK_size_t inFrameCount, PPixHand* outFrame) = 0;
+
+
+protected:
+	template<typename T>
+	prSuiteError GetProperty(
+		const char* inKey,
+		T& outValue)
+	{
+		PrMemoryPtr buffer;
+		prSuiteError suiteError = mVideoSegmentSuite->GetNodeProperty(mNodeID, inKey, &buffer);
+		if (PrSuiteErrorSucceeded(suiteError))
+		{
+			std::istringstream stream((const char*)buffer);
+			stream >> outValue;
+			mMemoryManagerSuite->PrDisposePtr(buffer);
+		}
+		return suiteError;
+	}
+
+	PrParam GetParam (csSDK_int32 inIndex, PrTime inTime);
+	const size_t RoundUp (size_t inValue, size_t inMultiple);
+
+	const int GetGPUBytesPerPixel (const PrPixelFormat inPixelFormat);
+
+	SPBasicSuite* mBasicSite;
+	PrSDKGPUDeviceSuite* mGPUDeviceSuite;
+	PrSDKGPUImageProcessingSuite* mGPUImageProcessingSuite;
+	PrSDKMemoryManagerSuite* mMemoryManagerSuite;
+	PrSDKPPixSuite* mPPixSuite;
+	PrSDKPPix2Suite* mPPix2Suite;
+	PrSDKVideoSegmentSuite* mVideoSegmentSuite;
+
+	piSuitesPtr mSuites;
+	PrTimelineID mTimelineID;
+	csSDK_int32 mNodeID;
+	csSDK_uint32 mDeviceIndex;
+	PrGPUDeviceInfo mDeviceInfo;
+
+private:
+	static std::atomic<uint32_t>objCnt;
+};
+
+/**
+**
+*/
+template<
+	class GPUFilter>
+	struct PrGPUFilterModule
+{
+	/**
+	**
+	*/
+	static prSuiteError Startup(
+		piSuitesPtr piSuites,
+		csSDK_int32* ioIndex,
+		PrGPUFilterInfo* outFilterInfo)
+	{
+		csSDK_int32 index = *ioIndex;
+		if (index + 1 > GPUFilter::PluginCount())
+		{
+			return suiteError_InvalidParms;
+		}
+		if (index + 1 < GPUFilter::PluginCount())
+		{
+			*ioIndex += 1;
+		}
+
+		outFilterInfo->outMatchName = GPUFilter::MatchName(piSuites, index);
+		outFilterInfo->outInterfaceVersion = PrSDKGPUFilterInterfaceVersion;
+
+		return GPUFilter::Startup(piSuites, *ioIndex);
+	}
+
+	/**
+	**
+	*/
+	static prSuiteError Shutdown(
+		piSuitesPtr piSuites,
+		csSDK_int32* ioIndex)
+	{
+		return GPUFilter::Shutdown(piSuites, *ioIndex);
+	}
+
+	/**
+	**
+	*/
+	static prSuiteError CreateInstance(
+		PrGPUFilterInstance* ioInstanceData)
+	{
+		GPUFilter* gpuFilter = new GPUFilter();
+		prSuiteError result = gpuFilter->Initialize(ioInstanceData);
+		if (PrSuiteErrorSucceeded(result))
+		{
+			ioInstanceData->ioPrivatePluginData = gpuFilter;
+		}
+		else
+		{
+			delete gpuFilter;
+		}
+		return result;
+	}
+
+	/**
+	**
+	*/
+	static prSuiteError DisposeInstance(
+		PrGPUFilterInstance* ioInstanceData)
+	{
+		delete (GPUFilter*)ioInstanceData->ioPrivatePluginData;
+		ioInstanceData->ioPrivatePluginData = 0;
+		return suiteError_NoError;
+	}
+
+	/**
+	**
+	*/
+	static prSuiteError GetFrameDependencies(
+		PrGPUFilterInstance* inInstanceData,
+		const PrGPUFilterRenderParams* inRenderParams,
+		csSDK_int32* ioQueryIndex,
+		PrGPUFilterFrameDependency* outFrameRequirements)
+	{
+		return ((GPUFilter*)inInstanceData->ioPrivatePluginData)->GetFrameDependencies(inRenderParams, ioQueryIndex, outFrameRequirements);
+	}
+
+	/**
+	**
+	*/
+	static prSuiteError Precompute(
+		PrGPUFilterInstance* inInstanceData,
+		const PrGPUFilterRenderParams* inRenderParams,
+		csSDK_int32 inIndex,
+		PPixHand inFrame)
+	{
+		return ((GPUFilter*)inInstanceData->ioPrivatePluginData)->Precompute(inRenderParams, inIndex, inFrame);
+	}
+
+	/**
+	**
+	*/
+	static prSuiteError Render(
+		PrGPUFilterInstance* inInstanceData,
+		const PrGPUFilterRenderParams* inRenderParams,
+		const PPixHand* inFrames,
+		csSDK_size_t inFrameCount,
+		PPixHand* outFrame)
+	{
+		return ((GPUFilter*)inInstanceData->ioPrivatePluginData)->Render(inRenderParams, inFrames, inFrameCount, outFrame);
+	}
+};
---
+++ AFTEREFFECT_PLUGIN.DEV/CommonGPULib/src/ImageLab2GpuObj.cpp
@@ -0,0 +1,147 @@
+#include "ImageLab2GpuObj.hpp"
+
+std::atomic<uint32_t>CImageLab2GpuObj::objCnt = 0u;
+
+CImageLab2GpuObj::CImageLab2GpuObj()
+{
+	objCnt++;
+
+	mGPUDeviceSuite = nullptr;
+	mGPUImageProcessingSuite = nullptr;
+	mMemoryManagerSuite = nullptr;
+	mPPixSuite = nullptr;
+	mPPix2Suite = nullptr;
+	mVideoSegmentSuite = nullptr;
+	mSuites = nullptr;
+
+	mTimelineID = -1;
+	mNodeID = -1;
+	mDeviceIndex = 0;
+	memset (&mDeviceInfo, 0, sizeof(mDeviceInfo));
+
+	return;
+}
+
+CImageLab2GpuObj::~CImageLab2GpuObj()
+{
+	objCnt--;
+	return;
+}
+
+
+prSuiteError CImageLab2GpuObj::Initialize (PrGPUFilterInstance* ioInstanceData)
+{
+	prSuiteError err = suiteError_IDNotValid;
+
+	if (nullptr != ioInstanceData)
+	{
+		const piSuitesPtr mSuites = ioInstanceData->piSuites;
+
+		if (nullptr != mSuites)
+		{
+			const PlugUtilFuncsPtr pUtilFuncs = mSuites->utilFuncs;
+
+			if (nullptr != pUtilFuncs)
+			{
+				mBasicSite = pUtilFuncs->getSPBasicSuite();
+
+				if (nullptr != mBasicSite)
+				{
+					const SPErr errGpuDev = 
+						mBasicSite->AcquireSuite (kPrSDKGPUDeviceSuite, kPrSDKGPUDeviceSuiteVersion, (const void**)&mGPUDeviceSuite);
+
+					const SPErr errGpuPrc = 
+						mBasicSite->AcquireSuite (kPrSDKGPUImageProcessingSuite, kPrSDKGPUImageProcessingSuiteVersion, (const void**)&mGPUImageProcessingSuite);
+
+					const SPErr errMemMng = 
+						mBasicSite->AcquireSuite (kPrSDKMemoryManagerSuite, kPrSDKMemoryManagerSuiteVersion, (const void**)&mMemoryManagerSuite);
+
+					const SPErr errPpxSit =
+						mBasicSite->AcquireSuite (kPrSDKPPixSuite, kPrSDKPPixSuiteVersion, (const void**)&mPPixSuite);
+
+					const SPErr errPpx2Sit =
+						mBasicSite->AcquireSuite (kPrSDKPPix2Suite, kPrSDKPPix2SuiteVersion, (const void**)&mPPix2Suite);
+
+					const SPErr errVidSeg =
+						mBasicSite->AcquireSuite(kPrSDKVideoSegmentSuite, kPrSDKVideoSegmentSuiteVersion, (const void**)&mVideoSegmentSuite);
+
+					mTimelineID = ioInstanceData->inTimelineID;
+					mNodeID = ioInstanceData->inNodeID;
+					mDeviceIndex = ioInstanceData->inDeviceIndex;
+
+					mGPUDeviceSuite->GetDeviceInfo(kPrSDKGPUDeviceSuiteVersion, mDeviceIndex, &mDeviceInfo);
+
+					/* start sites validation */
+					if (suiteError_NoError == errGpuDev && suiteError_NoError == errGpuPrc  && suiteError_NoError == errMemMng &&
+						suiteError_NoError == errPpxSit && suiteError_NoError == errPpx2Sit && suiteError_NoError == errVidSeg)
+					{
+						err = suiteError_NoError;
+					}
+				}
+			}
+		}
+
+	}
+	return err;
+}
+
+
+prSuiteError CImageLab2GpuObj::Cleanup(void)
+{
+	if (nullptr != mBasicSite)
+	{
+		mGPUDeviceSuite = nullptr;
+		mBasicSite->ReleaseSuite (kPrSDKGPUDeviceSuite, kPrSDKGPUDeviceSuiteVersion);
+
+		mGPUImageProcessingSuite = nullptr;
+		mBasicSite->ReleaseSuite (kPrSDKGPUImageProcessingSuite, kPrSDKGPUImageProcessingSuiteVersion);
+
+		mMemoryManagerSuite = nullptr;
+		mBasicSite->ReleaseSuite (kPrSDKMemoryManagerSuite, kPrSDKMemoryManagerSuiteVersion);
+
+		mPPixSuite = nullptr;
+		mBasicSite->ReleaseSuite (kPrSDKPPixSuite, kPrSDKPPixSuiteVersion);
+
+		mPPix2Suite = nullptr;
+		mBasicSite->ReleaseSuite (kPrSDKPPix2Suite, kPrSDKPPix2SuiteVersion);
+
+		mVideoSegmentSuite = nullptr;
+		mBasicSite->ReleaseSuite (kPrSDKVideoSegmentSuite, kPrSDKVideoSegmentSuiteVersion);
+
+		memset(&mDeviceInfo, 0, sizeof(mDeviceInfo));
+
+		mBasicSite = nullptr;
+	}
+
+	return suiteError_NoError;
+}
+
+
+prSuiteError CImageLab2GpuObj::GetFrameDependencies(const PrGPUFilterRenderParams* inRenderParams, csSDK_int32* ioQueryIndex, PrGPUFilterFrameDependency* outFrameRequirements)
+{
+	return suiteError_NotImplemented;
+}
+
+prSuiteError CImageLab2GpuObj::Precompute(const PrGPUFilterRenderParams* inRenderParams, csSDK_int32 inIndex, PPixHand inFrame)
+{
+	return suiteError_NotImplemented;
+}
+
+const int CImageLab2GpuObj::GetGPUBytesPerPixel (const PrPixelFormat inPixelFormat)
+{
+	return (PrPixelFormat_GPU_BGRA_4444_32f == inPixelFormat ? 16 : 8);
+}
+
+
+PrParam CImageLab2GpuObj::GetParam (csSDK_int32 inIndex, PrTime inTime)
+{
+	PrParam param {};
+	inIndex -= 1; // GPU filters do not include the input frame
+	mVideoSegmentSuite->GetParam (mNodeID, inIndex, inTime, &param);
+	return param;
+}
+
+const size_t CImageLab2GpuObj::RoundUp (size_t inValue, size_t inMultiple)
+{
+	return inValue ? ((inValue + inMultiple - 1) / inMultiple) * inMultiple : 0;
+}
