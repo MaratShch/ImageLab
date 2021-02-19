90c9787491bdf35a77a3cf9ee1f0e71501890791
Marat Shchuchinsky <maratshch@gmail.com>
2021-02-20 01:13:05 +0200

    add sRGB <-> HSI color convert

--- AFTEREFFECT_PLUGIN.DEV/ColorCorrectionHSL/include/ColorConverts.hpp
+++ AFTEREFFECT_PLUGIN.DEV/ColorCorrectionHSL/include/ColorConverts.hpp
@@ -58,13 +58,14 @@ inline void sRgb2hsl(const float& R, const float& G, const float& B, float& H, f
 
 inline void hsl2sRgb(const float& H, const float& S, const float& L, float& R, float& G, float& B) noexcept
 {
+	constexpr float denom = 1.f / 1e7f;
 	constexpr float reciproc3 = 1.0f / 3.0f;
 	constexpr float reciproc360 = 1.0f / 360.f;
 
 	float rR, gG, bB;
 
 	/* back convert to RGB space */
-	if (0.f == S)
+	if (denom >= S)
 	{
 		rR = gG = bB = L;
 	}
@@ -96,19 +97,17 @@ inline void hsl2sRgb(const float& H, const float& S, const float& L, float& R, f
 
 inline void sRgb2hsv(const float& R, const float& G, const float& B, float& H, float& S, float& V) noexcept
 {
+	constexpr float denom = 1.f / 1e7f;
+
 	/* start convert RGB to HSV color space */
-	float const maxVal = MAX3_VALUE(R, G, B);
-	float const minVal = MIN3_VALUE(R, G, B);
-	float const C = maxVal - minVal;
+	float const& maxVal = MAX3_VALUE(R, G, B);
+	float const& minVal = MIN3_VALUE(R, G, B);
+	float const& C = maxVal - minVal;
 
 	float const& value = maxVal;
 	float hue, saturation;
 
-	if (0.f == C)
-	{
-		hue = saturation = 0.f;
-	}
-	else
+	if (denom < C)
 	{
 		if (R == maxVal)
 		{
@@ -124,6 +123,10 @@ inline void sRgb2hsv(const float& R, const float& G, const float& B, float& H, f
 		hue *= 60.f;
 		saturation = C / maxVal;
 	}
+	else
+	{
+		hue = saturation = 0.f;
+	}
 
 	H = hue;
 	S = saturation;
@@ -137,45 +140,44 @@ inline void hsv2sRgb(const float& H, const float& S, const float& V, float& R, f
 {
 	auto const& c = S * V;
 	auto const& minV = V - c;
-	float h = H;
 
 	constexpr float reciproc360 = 1.0f / 360.f;
 	constexpr float reciproc60 = 1.0f  / 60.f;
 
-	h -= 360.f * floor(h * reciproc360);
-	h *= reciproc60;
+	float const& h = (H - 360.f * floor(H * reciproc360)) * reciproc60;
 	float const& X = c * (1.0f - fabs(h - 2.0f * floor(h * 0.5f) - 1.0f));
-
-	float fR, fG, fB;
 	const int& hh = static_cast<int const>(h);
 
-	if (hh < 3)
+	float fR, fG, fB;
+
+	if (hh & 0x1)
+	{
+		if (1 == hh)
+		{
+			fR = minV + X; fG = minV + c; fB = minV; /* 1 */
+		}
+		else if (3 == hh)
+		{
+			fR = minV; fG = minV + X; fB = minV + c; /* 3 */
+		}
+		else
+		{
+			fR = minV + c; fG = minV; fB = minV + X; /* 5 */
+		}
+	}
+	else
 	{
 		if (0 == hh)
 		{
-			fR = minV + c; fG = minV + X; fB = minV;
+			fR = minV + c; fG = minV + X; fB = minV; /* 0 */
 		}
-		else if (1 == hh)
+		else if (2 == hh)
 		{
-			fR = minV + X; fG = minV + c; fB = minV;
-		}
-		else
-		{
-			fR = minV; fG = minV + c; fB = minV + X;
-		}
-	}
-	else {
-		if (3 == hh)
-		{
-			fR = minV; fG = minV + X; fB = minV + c;
-		}
-		else if (4 == hh)
-		{
-			fR = minV + X; fG = minV; fB = minV + c;
+			fR = minV; fG = minV + c; fB = minV + X; /* 2 */
 		}
 		else
 		{
-			fR = minV + c; fG = minV; fB = minV + X;
+			fR = minV + X; fG = minV; fB = minV + c; /* 4 */
 		}
 	}
 
@@ -185,3 +187,76 @@ inline void hsv2sRgb(const float& H, const float& S, const float& V, float& R, f
 
 	return;
 }
+
+
+inline void sRgb2hsi(const float& R, const float& G, const float& B, float& H, float& S, float& I) noexcept
+{
+	constexpr float reciproc3 = 1.f / 3.f;
+	constexpr float denom = 1.f / 1e7f;
+	constexpr float reciprocPi180 = 180.f / 3.14159265f;
+
+	float i = (R + G + B) * reciproc3;
+	float h, s;
+
+	if (i > denom)
+	{
+		auto const& alpha = 0.5f * (2.f * R - G - B) + denom;
+		auto const& beta = 0.8660254037f * (G - B) + denom;
+		s = 1.f - MIN3_VALUE(R, G, B) / i;
+		h = atan2(beta, alpha) * reciprocPi180;
+		if (h < 0)
+			h += 360.f;
+	}
+	else
+	{
+		i = h = s = 0.f;
+	}
+
+	H = h;
+	S = s;
+	I = i;
+
+	return;
+}
+
+
+inline void hsi2sRgb(const float& H, const float& S, const float& I, float& R, float& G, float& B) noexcept
+{
+	constexpr float PiDiv180 = 3.14159265f / 180.f;
+	constexpr float reciproc360 = 1.0f / 360.f;
+	constexpr float denom = 1.f / 1e7f;
+
+	float h =  H - 360.f * floor (H * reciproc360);
+	const float& val1 = I * (1.f - S);
+	const float& tripleI = 3.f * I;
+
+	if (h < 120.f)
+	{
+		const float& cosTmp = cos((60.f - h) * PiDiv180);
+		const float& cosDiv = (0.f == cosTmp) ? denom : cosTmp;
+		B = val1;
+		R = I * (1.f + S * cos(h * PiDiv180) / cosDiv);
+		G = tripleI - R - B;
+	}
+	else if (h < 240.f)
+	{
+		h -= 120.f;
+		const float& cosTmp = cos((60.f - h) * PiDiv180);
+		const float& cosDiv = (0.f == cosTmp) ? denom : cosTmp;
+		R = val1;
+		G = I * (1.f + S * cos(h * PiDiv180) / cosDiv);
+		B = tripleI - R - G;
+	}
+	else
+	{
+		h -= 240.f;
+		const float& cosTmp = cos((60.f - h) * PiDiv180);
+		const float& cosDiv = (0.f == cosTmp) ? denom : cosTmp;
+		G = val1;
+		B = I * (1.f + S * cos(h * PiDiv180) / cosDiv);
+		R = tripleI - G - B;
+	}
+
+	return;
+}
+
