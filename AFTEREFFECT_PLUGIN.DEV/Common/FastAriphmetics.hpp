b63896ad48aba1f45a9dd251ed4ec83e1ec5f718
Marat Shchuchinsky <maratshch@gmail.com>
2021-06-26 18:38:59 +0300

    add fast Log computation for scalar and for AVX2

--- AFTEREFFECT_PLUGIN.DEV/Common/FastAriphmetics.hpp
+++ AFTEREFFECT_PLUGIN.DEV/Common/FastAriphmetics.hpp
@@ -107,6 +107,45 @@ namespace FastCompute
 		return (val + log_2);
 	}
 
+#if !defined __INTEL_COMPILER
+    #pragma warning( push )
+    #pragma warning( disable : 4244 )
+#endif
+	inline int __float_as_int (const float& in) noexcept
+	{
+		union fi { int i; float f; } conv;
+		conv.f = in;
+		return conv.i;
+	}
+
+	inline int __int_as_float(const int& in) noexcept
+	{
+		union fi { int i; float f; } conv;
+		conv.i = in;
+		return conv.f;
+	}
+
+	inline float Log (const float& a) noexcept
+	{
+		float m, r, s, t, i, f;
+		int32_t e;
+
+		e = (__float_as_int(a) - 0x3f2aaaab) & 0xff800000;
+		m = __int_as_float(__float_as_int(a) - e);
+		i = static_cast<float>(e) * 1.19209290e-7f; 
+		f = m - 1.0f;
+		s = f * f;
+		r = fmaf(0.230836749f, f, -0.279208571f);
+		t = fmaf(0.331826031f, f, -0.498910338f);
+		r = fmaf(r, s, t);
+		r = fmaf(r, s, f);
+		r = fmaf(i, 0.693147182f, r);
+		return r;
+	}
+#if !defined __INTEL_COMPILER
+    #pragma warning( pop )
+#endif
+
 	inline float Acos (float x) noexcept
 	{
 		const float& negate = float(x < 0.f);
@@ -231,5 +270,38 @@ namespace FastCompute
 		constexpr long double HalfPI = 3.14159265358979323846264l / 2.l;
 		return Cos(x + HalfPI);
 	}
+
+	namespace AVX2
+	{
+		inline __m256 mm256_fmaf(__m256 a, __m256 b, __m256 c) noexcept
+		{
+			return _mm256_add_ps(_mm256_mul_ps(a, b), c);
+		}
+
+		/* https://stackoverflow.com/questions/39821367/very-fast-approximate-logarithm-natural-log-function-in-c  */
+		inline __m256 Log(__m256 a) noexcept
+		{
+			__m256i aInt = *(__m256i*)(&a);
+			__m256i e = _mm256_sub_epi32(aInt, _mm256_set1_epi32(0x3f2aaaab));
+			e = _mm256_and_si256(e, _mm256_set1_epi32(0xff800000));
+
+			__m256i subtr = _mm256_sub_epi32(aInt, e);
+			__m256 m = *(__m256*)&subtr;
+
+			__m256 i = _mm256_mul_ps(_mm256_cvtepi32_ps(e), _mm256_set1_ps(1.19209290e-7f));// 0x1.0p-23
+																							/* m in [2/3, 4/3] */
+			__m256 f = _mm256_sub_ps(m, _mm256_set1_ps(1.0f));
+			__m256 s = _mm256_mul_ps(f, f);
+			/* Compute log1p(f) for f in [-1/3, 1/3] */
+			__m256 r = mm256_fmaf(_mm256_set1_ps(0.230836749f), f, _mm256_set1_ps(-0.279208571f));// 0x1.d8c0f0p-3, -0x1.1de8dap-2
+			__m256 t = mm256_fmaf(_mm256_set1_ps(0.331826031f), f, _mm256_set1_ps(-0.498910338f));// 0x1.53ca34p-2, -0x1.fee25ap-2
+
+			r = mm256_fmaf(r, s, t);
+			r = mm256_fmaf(r, s, f);
+			r = mm256_fmaf(i, _mm256_set1_ps(0.693147182f), r);  // 0x1.62e430p-1 // log(2)
+			return r;
+		}
+
+	} /* namespace AVX2 */
 
 }; /* namespace FastCompute */
