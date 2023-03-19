3d079479606ab49dda80251102b73204c1bf5624
Marat Shchuchinsky <maratshch@gmail.com>
2023-03-19 21:19:58 +0200

    reduce stack usage

--- AFTEREFFECT_PLUGIN.DEV/ImageEqualization/src/Equalization_Linear.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageEqualization/src/Equalization_Linear.cpp
@@ -68,12 +68,6 @@ PF_Err PR_ImageEq_Linear_VUYA_4444_32f_709
 	constexpr uint32_t histSize = static_cast<int32_t>(u16_value_white + 1);
 	constexpr uint32_t noiseLevel = 1;
 
-	uint32_t histIn[histSize]  = {};
-	uint32_t histOut[histSize] = {};
-	uint32_t histBin[histSize] = {};
-	uint32_t cumSum[histSize]  = {};
-	float    lut[histSize]     = {};
-
 	const PF_LayerDef*       __restrict pfLayer  = reinterpret_cast<const PF_LayerDef* __restrict>(&params[IMAGE_EQUALIZATION_FILTER_INPUT]->u.ld);
 	const PF_Pixel_VUYA_32f* __restrict localSrc = reinterpret_cast<const PF_Pixel_VUYA_32f* __restrict>(pfLayer->data);
 	      PF_Pixel_VUYA_32f* __restrict localDst = reinterpret_cast<PF_Pixel_VUYA_32f* __restrict>(output->data);
@@ -82,17 +76,25 @@ PF_Err PR_ImageEq_Linear_VUYA_4444_32f_709
 	auto const width  = pfLayer->extent_hint.right  - pfLayer->extent_hint.left;
 	auto const line_pitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_BGRA_32f_size);
 
-	/* create histogram of the luminance channel [32 floating point pixel values converted to integer] */
-	AVX2::Histogram::make_luma_histogram_VUYA4444_32f(localSrc, histIn, histSize, width, height, line_pitch);
-	/* make histogram binarization */
-	AVX2::Histogram::make_histogram_binarization(histIn, histBin, histSize, noiseLevel);
-	/* make cumulative SUM of binary histogram elements */
-	AVX2::MiscUtils::cum_sum_uint32(histBin, histOut, histSize);
-	/* generate LUT */
-	constexpr int32_t lastHistElem = histSize - 1;
-	const float coeff = static_cast<float>(lastHistElem) / static_cast<float>(histOut[lastHistElem]);
-	AVX2::MiscUtils::generate_lut_float32 (histOut, lut, coeff, histSize);
+	CACHE_ALIGN uint32_t histBin[histSize] = {};
+	CACHE_ALIGN float    lut    [histSize] = {};
 
+	{
+		CACHE_ALIGN uint32_t histIn[histSize] = {};
+		/* create histogram of the luminance channel [32 floating point pixel values converted to integer] */
+		AVX2::Histogram::make_luma_histogram_VUYA4444_32f(localSrc, histIn, histSize, width, height, line_pitch);
+		/* make histogram binarization */
+		AVX2::Histogram::make_histogram_binarization(histIn, histBin, histSize, noiseLevel);
+	}
+	{
+		CACHE_ALIGN uint32_t histOut[histSize] = {};
+		/* make cumulative SUM of binary histogram elements */
+		AVX2::MiscUtils::cum_sum_uint32(histBin, histOut, histSize);
+		/* generate LUT */
+		constexpr int32_t lastHistElem = histSize - 1;
+		const float coeff = static_cast<float>(lastHistElem) / static_cast<float>(histOut[lastHistElem]);
+		AVX2::MiscUtils::generate_lut_float32(histOut, lut, coeff, histSize);
+	}
 	/* apply LUT to the image */
 	imgApplyLut(localSrc, localDst, lut, width, height, line_pitch, line_pitch);
 
