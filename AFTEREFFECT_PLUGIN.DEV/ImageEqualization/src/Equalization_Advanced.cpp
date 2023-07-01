4550caa95e1a136f13c5b62763548530fd61c352
Marat Shchuchinsky <maratshch@gmail.com>
2023-07-02 00:09:37 +0300

    dev. commit

--- AFTEREFFECT_PLUGIN.DEV/ImageEqualization/src/Equalization_Advanced.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageEqualization/src/Equalization_Advanced.cpp
@@ -74,7 +74,7 @@ PF_Err PR_ImageEq_Advanced_BGRA_4444_16u
 ) noexcept
 {
 	A_long i, j;
-	const PF_LayerDef*      __restrict pfLayer = reinterpret_cast<const PF_LayerDef* __restrict>(&params[IMAGE_EQUALIZATION_FILTER_INPUT]->u.ld);
+	const PF_LayerDef*      __restrict pfLayer   = reinterpret_cast<const PF_LayerDef* __restrict>(&params[IMAGE_EQUALIZATION_FILTER_INPUT]->u.ld);
 	const PF_Pixel_BGRA_16u* __restrict localSrc = reinterpret_cast<const PF_Pixel_BGRA_16u* __restrict>(pfLayer->data);
 	      PF_Pixel_BGRA_16u* __restrict localDst = reinterpret_cast<      PF_Pixel_BGRA_16u* __restrict>(output->data);
 
@@ -82,8 +82,10 @@ PF_Err PR_ImageEq_Advanced_BGRA_4444_16u
 	auto const sizeX = pfLayer->extent_hint.right  - pfLayer->extent_hint.left;
 	auto const line_pitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_BGRA_16u_size);
 
-	const size_t frameSize = sizeY * FastCompute::Abs(line_pitch);
-	const size_t requiredMemSize = CreateAlignment(frameSize * fCIELabPix_size, static_cast<size_t>(CACHE_LINE));
+	constexpr size_t components_size = fCIELabPix_size /* CIELab buffer */ + fXYZPix_size /* XYZ buffer */ + sizeof(float)/* cs_out buffer */;
+	const size_t frameSize = sizeX * sizeY;
+	const size_t memSizeForTmpBuffers = frameSize * components_size;
+	const size_t requiredMemSize = CreateAlignment(memSizeForTmpBuffers, static_cast<size_t>(CACHE_LINE));
 
 	/* Get memory block */
 	void* pMemoryBlock = nullptr;
@@ -91,17 +93,28 @@ PF_Err PR_ImageEq_Advanced_BGRA_4444_16u
 
 	if (-1 != blockId && nullptr != pMemoryBlock)
 	{
-		constexpr float scale2fRGB = 1.f / static_cast<float>(u16_value_white);
+		constexpr float scale = static_cast<float>(u16_value_white);
+		constexpr float scale2sRGB = 1.f / scale;
 		const float* __restrict fReferences = cCOLOR_ILLUMINANT[observer_CIE_1931][color_ILLUMINANT_D65];
+
 		fCIELabPix* srcLabBuffer = reinterpret_cast<fCIELabPix*>(pMemoryBlock);
-		uint32_t idx = 0u;
+		float*      cs_out = reinterpret_cast<float*     >(srcLabBuffer + frameSize);
+		fXYZPix*    xyz_buffer = reinterpret_cast<fXYZPix*   >(cs_out + frameSize);
+
+		constexpr float lightness_enhanecemnt = 1.25f;
 
 #ifdef _DEBUG
 		memset(pMemoryBlock, 0, requiredMemSize);
 #endif // _DEBUG
 
 		/* convert from RGB to CIEL*a*b color space */
-//		ConvertToLabColorSpace (localSrc, srcLabBuffer, sizeX, sizeY, line_pitch, FastCompute::Abs(sizeX), scale2fRGB);
+		const float cs_out_max = ConvertToLabColorSpace(localSrc, srcLabBuffer, cs_out, sizeX, sizeY, line_pitch, sizeX, scale2sRGB);
+
+		/* improve */
+		fCIELabHueImprove(srcLabBuffer, xyz_buffer, cs_out, sizeX, sizeY, 85.f, lightness_enhanecemnt, cs_out_max);
+
+		/* convert improved XYZ to RGB */
+		ImprovedImageRestore(localSrc, localDst, xyz_buffer, sizeX, sizeY, line_pitch, sizeX, line_pitch, scale);
 
 		/* release memory block */
 		::FreeMemoryBlock(blockId);
--- AFTEREFFECT_PLUGIN.DEV/ImageEqualization/src/Equalization_AdvancedEx.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageEqualization/src/Equalization_AdvancedEx.cpp
@@ -64,13 +64,10 @@ void fCIELabHueImprove
 	constexpr float ligh_reciproc_100 = 1.f / 100.f;
 
 	for (A_long i = 0; i < imgSize; i++)
-	{                                 // DBG:
-		auto const& L = pLabSrc[i].L; // 51.77
-		auto const& a = pLabSrc[i].a; // -4.54
-		auto const& b = pLabSrc[i].b; // -30.41
-//		auto const L = 51.77f;
-//		auto const a = -4.54f;
-//		auto const b = -30.41f;
+	{                                 
+		auto const& L = pLabSrc[i].L; 
+		auto const& a = pLabSrc[i].a; 
+		auto const& b = pLabSrc[i].b; 
 		const float wc = tone_map_inc (cs_out[i] * reciprocCsOutMax);
 		const float hangle_out2 = FastCompute::Atan2d(b, a) * reciproc180;
 		constexpr float alpha = 10.f;
@@ -81,7 +78,6 @@ void fCIELabHueImprove
 		float pLabSrcB = wx * b;
 		/* lightness enhanecemnt */
 		float pLabSrcL = 100.f * FastCompute::Pow (L * ligh_reciproc_100, lightness_reciproc);
-//		float pLabSrcL = 100.f * std::pow(L * ligh_reciproc_100, lightness_reciproc);
 
 		const float csOut = FastCompute::Sqrt (pLabSrcA * pLabSrcA + pLabSrcB * pLabSrcB);
 		const float h_out  = ((csOut >= 0.1f) ? pLabSrcB / pLabSrcA : 0.f);
@@ -97,12 +93,17 @@ void fCIELabHueImprove
 		constexpr float reciproc7x = 0.9505f / 7.78f;
 		constexpr float reciproc7y = 1.f / 7.78f;
 		constexpr float reciproc7z = 1.089f / 7.78f;
-		float X = (fX > 0.20689f ? (0.9505f * fX * fX * fX) : ((fX - reciproc16) * reciproc7x));
-		float Y = (fY > 0.20689f ? (fY * fY * fY)           :  (fY - reciproc16) * reciproc7y);
-		float Z = (fZ > 0.20689f ? (1.089f * fZ * fZ * fZ)  :  (fZ - reciproc16) * reciproc7z);
+		float X = (fX > 0.20689f ? (0.9505f * fX * fX * fX) : (fX - reciproc16) * reciproc7x);
+		float Y = (fY > 0.20689f ? (fY * fY * fY)           : (fY - reciproc16) * reciproc7y);
+		float Z = (fZ > 0.20689f ? (1.0890f * fZ * fZ * fZ) : (fZ - reciproc16) * reciproc7z);
 
-		const int32_t indexY = FastCompute::Min(100, static_cast<const int32_t>(100.f * Y));	/* 0 ... 100 */
-		const int32_t indexH = FastCompute::Min(360, static_cast<const int32_t>(hangle_out));	/* 0 ... 360 */
+		constexpr int32_t maxIndexY = 100;
+		constexpr int32_t maxIndexH = 360;
+		constexpr int32_t minIndexY = 0;
+		constexpr int32_t minIndexH = 0;
+
+		const int32_t indexY = FastCompute::Min(maxIndexY - 1, static_cast<const int32_t>(100.f * Y));	/* 0 ... 100 */
+		const int32_t indexH = FastCompute::Min(maxIndexH - 1, static_cast<const int32_t>(hangle_out));	/* 0 ... 360 */
 
 		constexpr float Xn = 0.9505f;
 		constexpr float Zn = 1.0890f;
@@ -126,29 +127,27 @@ void fCIELabHueImprove
 		}
 		else
 		{
-			if (0 == indexY || 100 == indexY)
+			if (minIndexY == indexY || maxIndexY == indexY)
 			{
-				if (0 == indexH || 360 == indexH)
-					fCsOut = FastCompute::Min(cstar_gmax[indexY], cstar_gmax[indexY * 359]);
+				if (minIndexH == indexH || maxIndexH == indexH)
+					fCsOut = FastCompute::Min(cstar_gmax[indexY], cstar_gmax[indexY * maxIndexH]);
 				else
-					fCsOut = FastCompute::Min(cstar_gmax[indexY + 100 * indexH], cstar_gmax[indexY + 100 * (indexH + 1)]);
+					fCsOut = FastCompute::Min(cstar_gmax[indexY + maxIndexY * indexH], cstar_gmax[indexY + maxIndexY * (indexH + 1)]);
 			}
-			else if (indexH == 0 || indexH == 360)
+			else if (minIndexH == indexH || maxIndexH == indexH)
 			{
-				const float cs = FastCompute::Min3(cstar_gmax[indexY], cstar_gmax[indexY + 1], cstar_gmax[indexY * 359]);
-				fCsOut = FastCompute::Min(cs, cstar_gmax[(indexY + 1) * 359]);
+				const float cs = FastCompute::Min3(cstar_gmax[indexY], cstar_gmax[indexY + 1], cstar_gmax[indexY * maxIndexH]);
+				fCsOut = FastCompute::Min(cs, cstar_gmax[(indexY + 1) * maxIndexH]);
 			}
 			else
 			{
-				const float cs = FastCompute::Min3(cstar_gmax[indexY + 100 * indexH], cstar_gmax[indexY + 1 + 100 * indexH], cstar_gmax[indexY + 100 * indexH]);
-				fCsOut = FastCompute::Min(cs, cstar_gmax[indexY + 1 + 100 * (1 + indexH)]);
+				const float cs = FastCompute::Min3(cstar_gmax[indexY + maxIndexY * indexH], cstar_gmax[indexY + 1 + maxIndexY * indexH], cstar_gmax[indexY + maxIndexY * indexH]);
+				fCsOut = FastCompute::Min(cs, cstar_gmax[indexY + 1 + maxIndexY * (1 + indexH)]);
 			}
 		}
 
 		fX =  sign(pLabSrcA) * fCsOut / (500.f * FastCompute::Sqrt(1.f + sq_h_out)) + fY;
 		fZ = -sign(pLabSrcB) * fCsOut / (200.f * FastCompute::Sqrt(1.f + 1.f / sq_h_out)) + fY;
-//		fX = sign(pLabSrcA) * fCsOut  / (500.f * std::sqrt(1.f + sq_h_out)) + fY;
-//		fZ = -sign(pLabSrcB) * fCsOut / (200.f * std::sqrt(1.f + 1.f / sq_h_out)) + fY;
 
 		xyz_out[i].X = (fX > 0.20689f) ? 0.9505f * fX * fX * fX : (fX - reciproc16) * reciproc7x;
 		xyz_out[i].Y = (fY > 0.20689f) ? fY * fY * fY           : (fY - reciproc16) * reciproc7y;
