b30b5eee9c5af02a236ec794479c6ed18fa895d5
Marat Shchuchinsky <maratshch@gmail.com>
2024-06-24 23:04:03 +0300

    implement 16 bits ARGB Median Filter for Ae [not tested yet]

--- AFTEREFFECT_PLUGIN.DEV/MedianFilter/src/MedianFilterInAe.cpp
+++ AFTEREFFECT_PLUGIN.DEV/MedianFilter/src/MedianFilterInAe.cpp
@@ -66,8 +66,52 @@ PF_Err MeadianFilterInAE_16bits
 	PF_LayerDef* output
 ) noexcept
 {
+	const PF_EffectWorld*   __restrict input = reinterpret_cast<const PF_EffectWorld* __restrict>(&params[MEDIAN_FILTER_INPUT]->u.ld);
+	uint64_t* __restrict localSrc = reinterpret_cast<uint64_t* __restrict>(input->data);
+	uint64_t* __restrict localDst = reinterpret_cast<uint64_t* __restrict>(output->data);
+
+	auto const& src_pitch = input->rowbytes  / static_cast<A_long>(PF_Pixel_ARGB_16u_size);
+	auto const& dst_pitch = output->rowbytes / static_cast<A_long>(PF_Pixel_ARGB_16u_size);
+
+	auto const& height = output->height;
+	auto const& width  = output->width;
+	auto const kernelSize = get_kernel_size(params);
 	PF_Err err = PF_Err_NONE;
-	return err;
+	bool medianResult = false;
+
+	constexpr A_long channelMaskL = 0xFFFF0000;
+	constexpr A_long channelMaskH = 0xFFFFFFFF;
+
+	switch (kernelSize)
+	{
+		case 0:
+			AEFX_SuiteScoper<PF_WorldTransformSuite1>(in_data, kPFWorldTransformSuite, kPFWorldTransformSuiteVersion1, out_data)->copy(in_data->effect_ref, &params[MEDIAN_FILTER_INPUT]->u.ld, output, NULL, NULL);
+			medianResult = true;
+		break;
+
+		case 3:
+		/* manually optimized variant 3x3 */
+			medianResult = AVX2::Median::median_filter_3x3_RGB_4444_16u (localSrc, localDst, height, width, src_pitch, dst_pitch, channelMaskL, channelMaskH);
+		break;
+
+		case 5:
+		/* manually optimized variant 5x5 */
+			medianResult = AVX2::Median::median_filter_5x5_RGB_4444_16u (localSrc, localDst, height, width, src_pitch, dst_pitch, channelMaskL, channelMaskH);
+		break;
+
+		case 7:
+		/* manually optimized variant 7x7 */
+			medianResult = AVX2::Median::median_filter_7x7_RGB_4444_16u (localSrc, localDst, height, width, src_pitch, dst_pitch, channelMaskL, channelMaskH);
+		break;
+
+		default:
+		/* median via histogramm algo */
+			medianResult = median_filter_constant_time_ARGB_4444_16u (localSrc, localDst, height, width, src_pitch, dst_pitch, kernelSize);
+		break;
+
+	}
+
+	return (true == medianResult ? PF_Err_NONE : PF_Err_INTERNAL_STRUCT_DAMAGED);
 }
 
 
