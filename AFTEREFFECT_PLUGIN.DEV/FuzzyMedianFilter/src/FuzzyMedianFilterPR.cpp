8d4c5f7033769098c5d58f0a9019fefade3c3b51
Marat Shchuchinsky <maratshch@gmail.com>
2024-10-15 12:32:16 +0300

    add empty AE plugin project for FuzzyMedianFilter for implementation in future

---
+++ AFTEREFFECT_PLUGIN.DEV/FuzzyMedianFilter/src/FuzzyMedianFilterPR.cpp
@@ -0,0 +1,56 @@
+#include "FuzzyMedianFilter.hpp"
+#include "PrSDKAESupport.h"
+
+
+PF_Err ProcessImgInPR
+(
+	PF_InData*   __restrict in_data,
+	PF_OutData*  __restrict out_data,
+	PF_ParamDef* __restrict params[],
+	PF_LayerDef* __restrict output
+) noexcept
+{
+	PF_Err err{ PF_Err_NONE };
+	PF_Err errFormat{ PF_Err_INVALID_INDEX };
+	PrPixelFormat destinationPixelFormat{ PrPixelFormat_Invalid };
+
+	/* This plugin called frop PR - check video fomat */
+	auto const& pixelFormatSuite{ AEFX_SuiteScoper<PF_PixelFormatSuite1>(in_data, kPFPixelFormatSuite, kPFPixelFormatSuiteVersion1, out_data) };
+
+	if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat)))
+	{
+		switch (destinationPixelFormat)
+		{
+			case PrPixelFormat_BGRA_4444_8u:
+			break;
+
+			case PrPixelFormat_BGRA_4444_16u:
+			break;
+
+			case PrPixelFormat_BGRA_4444_32f:
+			break;
+
+			case PrPixelFormat_VUYA_4444_8u_709:
+			case PrPixelFormat_VUYA_4444_8u:
+			break;
+
+			case PrPixelFormat_VUYA_4444_32f_709:
+			case PrPixelFormat_VUYA_4444_32f:
+			break;
+
+			case PrPixelFormat_RGB_444_10u:
+			break;
+
+			default:
+			break;
+		} /* switch (destinationPixelFormat) */
+
+	} /* if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat))) */
+	else
+	{
+		/* error in determine pixel format */
+		err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
+	}
+
+	return err;
+}
