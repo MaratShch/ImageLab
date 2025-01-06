39de0ac844ef65c77ee15386581702715e0a7b51
Marat Shchuchinsky <maratshch@gmail.com>
2025-01-06 02:26:50 +0200

    fix omission

--- AFTEREFFECT_PLUGIN.DEV/ColorSubstitution/ColorSubstitution.r
+++ AFTEREFFECT_PLUGIN.DEV/ColorSubstitution/ColorSubstitution.r
@@ -13,7 +13,7 @@ resource 'PiPL' (16000) {
 		},
 		/* [2] */
 		Name {
-			"Color Temperature"
+			"Color Substitution"
 		},
 		/* [3] */
 		Category {
@@ -60,7 +60,7 @@ resource 'PiPL' (16000) {
 		},
 		/* [11] */
 		AE_Effect_Match_Name {
-			"ImageLab2 Color Temperature"
+			"ImageLab2 Color Substitution"
 		},
 		/* [12] */
 		AE_Reserved_Info {
