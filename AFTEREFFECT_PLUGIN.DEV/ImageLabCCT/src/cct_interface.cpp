dd5b6431290408a6b2bc340bffa3324a08330f24
Marat Shchuchinsky <maratshch@gmail.com>
2025-03-21 01:50:35 +0200

    tmp

--- AFTEREFFECT_PLUGIN.DEV/ImageLabCCT/src/cct_interface.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageLabCCT/src/cct_interface.cpp
@@ -1,4 +1,6 @@
 #include "cct_interface.hpp"
+#include "CctLimits.hpp"
+
 
 using namespace AlgoCCT;
 
@@ -9,17 +11,19 @@ CctHandleF32::CctHandleF32()
 
 CctHandleF32::~CctHandleF32()
 {
-    m_Lut.clear();
     return;
 }
 
 bool CctHandleF32::Initialize(void)
 {
+    m_Lut = initLut (CCT_Limits::waveLenMin, CCT_Limits::waveLenMax, CCT_Limits::waveLenStep, CCT_Limits::cctMin, CCT_Limits::cctMax, CCT_Limits::cctStep);
+
     return true;
 }
 
 
 void CctHandleF32::Deinitialize(void)
 {
+    m_Lut.clear();
     return;
 }
