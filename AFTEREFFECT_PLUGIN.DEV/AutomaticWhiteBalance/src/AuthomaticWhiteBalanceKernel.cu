1ed2e2274ab29ed55511c14b6c81e243f4c0e793
Marat Shchuchinsky <maratshch@gmail.com>
2025-03-31 23:33:48 +0300

    tmp

--- AFTEREFFECT_PLUGIN.DEV/AutomaticWhiteBalance/src/AuthomaticWhiteBalanceKernel.cu
+++ AFTEREFFECT_PLUGIN.DEV/AutomaticWhiteBalance/src/AuthomaticWhiteBalanceKernel.cu
@@ -22,10 +22,12 @@ inline __device__ Pixel16 FloatToHalf4(float4 in) noexcept
 }
 
 
-inline __device__ const float4 rgb2yuv (const float4& rgb, const eCOLOR_SPACE& color_space) noexcept
+inline __device__ const float4 rgb2yuv
+(
+    const float4& rgb,  /* z = R, y = G, x = B, w = A */
+    const eCOLOR_SPACE& color_space
+) noexcept
 {
-    float4 out;
-
     float constexpr dev_RGB2YUV[][9] =
     {
         // BT.601
@@ -57,6 +59,18 @@ inline __device__ const float4 rgb2yuv (const float4& rgb, const eCOLOR_SPACE& c
         }
     };
 
+    float3 in;
+    float4 out;
+
+    in.x = rgb.x * 255.0f;
+    in.y = rgb.y * 255.0f;
+    in.z = rgb.z * 255.0f;
+
+    out.x = in.z * dev_RGB2YUV[color_space][0] + in.y * dev_RGB2YUV[color_space][1] + in.x * dev_RGB2YUV[color_space][2]; // Y
+    out.y = in.z * dev_RGB2YUV[color_space][3] + in.y * dev_RGB2YUV[color_space][4] + in.x * dev_RGB2YUV[color_space][5]; // U
+    out.z = in.z * dev_RGB2YUV[color_space][6] + in.y * dev_RGB2YUV[color_space][7] + in.x * dev_RGB2YUV[color_space][8]; // V
+    out.w = rgb.w; //copy Alpha channel directly from source pixel
+
     return out;
 }
 
@@ -71,15 +85,11 @@ void CollectRgbStatistics_CUDA
     int   srcPitch,
     int   dstPitch,
     int   is16f,
-    const eCOLOR_SPACE color_space
+    const eCOLOR_SPACE color_space,
+    const float gray_threshold
 )
 {
     float4 inPix;
-    float4 yuvPix;
-
-    // Shared memory for partial results
-    __shared__ float uPartialSum;
-    __shared__ float vPartialSum;
 
     const int x = blockIdx.x * blockDim.x + threadIdx.x;
     const int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -96,7 +106,8 @@ void CollectRgbStatistics_CUDA
         inPix = srcBuf[y * srcPitch + x];
     }
 
-    yuvPix = rgb2yuv (inPix, color_space);
+    const float4 yuvPix = rgb2yuv (inPix, color_space);
+    const float F = (std::abs(yuvPix.y) + std::abs(yuvPix.z)) / std::max(yuvPix.x, FLT_EPSILON);
 
     return;
 }
@@ -119,9 +130,9 @@ void AuthomaticWhiteBalance_CUDA
     unsigned int iter_cnt
 )
 {
-    float4* gpuTmpImage = nullptr;
-    float4* src = nullptr;
-    float4* dst = nullptr;
+    float4* RESTRICT gpuTmpImage = nullptr;
+    float4* RESTRICT src = nullptr;
+    float4* RESTRICT dst = nullptr;
     int srcIdx = 0, dstIdx = 1;
     int inPitch, outPitch;
     float uAvg, vAvg;
@@ -130,7 +141,7 @@ void AuthomaticWhiteBalance_CUDA
     dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y, 1);
 
     // allocate memory for intermediate processing results
-    const unsigned int blocksNumber = FastCompute::Min(2u, (iter_cnt - 1u));
+    const unsigned int blocksNumber = std::min(2u, (iter_cnt - 1u));
     if (blocksNumber > 0)
     {
         const unsigned int frameSize = static_cast<unsigned int>(width) * static_cast<unsigned int>(height);
@@ -147,18 +158,18 @@ void AuthomaticWhiteBalance_CUDA
         uAvg = vAvg = 0.f;
 
         if (0u == i)
-        {
+        {   // First iteration case
             dstIdx++;
             dstIdx &= 0x01;
-            src = reinterpret_cast<float4*>(inBuf);
-            dst = (1u == iter_cnt) ? reinterpret_cast<float4*>(outBuf) : gpuImage[dstIdx];
+            src = reinterpret_cast<float4* RESTRICT>(inBuf);
+            dst = (1u == iter_cnt) ? reinterpret_cast<float4* RESTRICT>(outBuf) : gpuImage[dstIdx];
             inPitch = srcPitch;
             outPitch = (1u == iter_cnt) ? destPitch : width;
         }
         else if ((iter_cnt - 1u) == i)
-        {
+        {   // Last iteration case
             src = gpuImage[dstIdx];
-            dst = reinterpret_cast<float4*>(outBuf);
+            dst = reinterpret_cast<float4* RESTRICT>(outBuf);
             inPitch = width;
             outPitch = destPitch;
         } /* if (k > 0) */
@@ -172,8 +183,7 @@ void AuthomaticWhiteBalance_CUDA
             inPitch = outPitch = width;
         }
 
-        CollectRgbStatistics_CUDA <<< gridDim, blockDim >>> (src, dst, width, height, inPitch, outPitch, is16f, color_space);
-
+        CollectRgbStatistics_CUDA <<< gridDim, blockDim >>> (src, dst, width, height, inPitch, outPitch, is16f, color_space, gray_threshold);
 
     } // for (unsigned int i = 0u; i < iter_cnt; i++)
 
