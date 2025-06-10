891dc0e8df4a8d9dd12ad8358da6651323c0903d
Marat Shchuchinsky <maratshch@gmail.com>
2025-06-11 01:26:52 +0300

    start implement FFT by Cooley-Tukey classical algorithm for usiage into ImageStylixation plugin

---
+++ AFTEREFFECT_PLUGIN.DEV/ImageLabProcLib/include/algo_fft_utils.hpp
@@ -0,0 +1,48 @@
+#ifndef __IMAGE_LAB_ALGORITHM_UTILS_FOR_FFT__
+#define __IMAGE_LAB_ALGORITHM_UTILS_FOR_FFT__
+
+#include <cstdint>
+
+
+inline uint32_t get_next_power_of_2 (uint32_t n) noexcept
+{
+    if (n == 0)
+        return 1;
+
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n++;
+    return n;
+}
+
+inline int32_t get_next_power_of_2 (int32_t n) noexcept
+{
+    if (n == 0)
+        return 1;
+
+    uint32_t un = static_cast<uint32_t>(n);
+    return static_cast<int32_t>(get_next_power_of_2(un));
+}
+
+
+inline void get_padded_image_size (const uint32_t& sizeX, const uint32_t& sizeY, uint32_t& paddedX, uint32_t& paddedY) noexcept
+{
+    paddedX = get_next_power_of_2 (sizeX);
+    paddedY = get_next_power_of_2 (sizeY);
+    return;
+}
+
+
+inline void get_padded_image_size(const int32_t& sizeX, const int32_t& sizeY, int32_t& paddedX, int32_t& paddedY) noexcept
+{
+    paddedX = get_next_power_of_2(sizeX);
+    paddedY = get_next_power_of_2(sizeY);
+    return;
+}
+
+
+#endif // __IMAGE_LAB_ALGORITHM_UTILS_FOR_FFT__
