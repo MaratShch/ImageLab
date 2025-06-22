7beb4be2965f823f0b6d8bfc171a157ae16b6a83
Marat Shchuchinsky <maratshch@gmail.com>
2025-06-23 01:22:02 +0300

    dev. commit

--- AFTEREFFECT_PLUGIN.DEV/ImageStabilizer/src/ImageStabilizerAE.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageStabilizer/src/ImageStabilizerAE.cpp
@@ -1,5 +1,5 @@
 #include "ImageStabilizer.hpp"
-
+#include "ImageStabilizerEnum.hpp"
 
 PF_Err ImageStabilizer_InAE_8bits
 (
@@ -23,6 +23,39 @@ PF_Err ImageStabilizer_InAE_16bits
 	return PF_Err_NONE;
 }
 
+PF_Err ImageStabilizer_InAE_32bits
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    return PF_Err_NONE;
+}
+
+
+inline PF_Err ImageStabilizer_InAE_DeepWorld
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    PF_Err	err = PF_Err_NONE;
+    PF_PixelFormat format = PF_PixelFormat_INVALID;
+    AEFX_SuiteScoper<PF_WorldSuite2> wsP = AEFX_SuiteScoper<PF_WorldSuite2>(in_data, kPFWorldSuite, kPFWorldSuiteVersion2, out_data);
+    if (PF_Err_NONE == wsP->PF_GetPixelFormat(reinterpret_cast<PF_EffectWorld* __restrict>(&params[IMAGE_STABILIZATION_INPUT]->u.ld), &format))
+    {
+        err = (format == PF_PixelFormat_ARGB128 ?
+            ImageStabilizer_InAE_32bits(in_data, out_data, params, output) : ImageStabilizer_InAE_16bits(in_data, out_data, params, output));
+    }
+    else
+        err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
+
+    return err;
+}
 
 PF_Err
 ProcessImgInAE
@@ -34,6 +67,6 @@ ProcessImgInAE
 ) noexcept
 {
 	return (PF_WORLD_IS_DEEP(output) ?
-		ImageStabilizer_InAE_16bits(in_data, out_data, params, output) :
+        ImageStabilizer_InAE_DeepWorld (in_data, out_data, params, output) :
 		ImageStabilizer_InAE_8bits (in_data, out_data, params, output));
 }
