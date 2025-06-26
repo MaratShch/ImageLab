d48ef92220c194d5c24db97a71fd4c7e45eb67ea
Marat Shchuchinsky <maratshch@gmail.com>
2025-06-27 00:34:23 +0300

    add ARGB_32f format for Ae for some plugins

--- AFTEREFFECT_PLUGIN.DEV/PerceptualColorCorrection/src/PerceptualColorCorrectionAe.cpp
+++ AFTEREFFECT_PLUGIN.DEV/PerceptualColorCorrection/src/PerceptualColorCorrectionAe.cpp
@@ -1,4 +1,6 @@
 #include "PerceptualColorCorrection.hpp"
+#include "PerceptualColorCorrectionEnum.hpp"
+
 
 PF_Err
 RenderInAfterEffect_8bits
@@ -27,6 +29,41 @@ RenderInAfterEffect_16bits
 
 
 PF_Err
+RenderInAfterEffect_32bits
+(
+    PF_InData*		in_data,
+    PF_OutData*		out_data,
+    PF_ParamDef*	params[],
+    PF_LayerDef*	output
+) noexcept
+{
+    return PF_Err_NONE;
+}
+
+
+inline PF_Err RenderInAfterEffect_DeepWorld
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    PF_Err	err = PF_Err_NONE;
+    PF_PixelFormat format = PF_PixelFormat_INVALID;
+    AEFX_SuiteScoper<PF_WorldSuite2> wsP = AEFX_SuiteScoper<PF_WorldSuite2>(in_data, kPFWorldSuite, kPFWorldSuiteVersion2, out_data);
+    if (PF_Err_NONE == wsP->PF_GetPixelFormat(reinterpret_cast<PF_EffectWorld* __restrict>(&params[PERCOLOR_CORRECTION_INPUT]->u.ld), &format))
+    {
+        err = (format == PF_PixelFormat_ARGB128 ?
+            RenderInAfterEffect_32bits(in_data, out_data, params, output) : RenderInAfterEffect_16bits(in_data, out_data, params, output));
+    }
+    else
+        err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
+
+    return err;
+}
+
+PF_Err
 RenderInAfterEffect
 (
 	PF_InData*		in_data,
@@ -36,6 +73,6 @@ RenderInAfterEffect
 )
 {
 	return (PF_WORLD_IS_DEEP(output) ?
-		RenderInAfterEffect_16bits(in_data, out_data, params, output) :
+        RenderInAfterEffect_DeepWorld (in_data, out_data, params, output) :
 		RenderInAfterEffect_8bits (in_data, out_data, params, output));
 }
