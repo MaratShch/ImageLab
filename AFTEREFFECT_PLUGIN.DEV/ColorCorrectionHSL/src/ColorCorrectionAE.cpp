1aa63c6b05e4ee80759840d352a7870b5cbddc97
Marat Shchuchinsky <maratshch@gmail.com>
2025-06-27 00:25:51 +0300

    add ARGB_32f format for Ae for some plugins

--- AFTEREFFECT_PLUGIN.DEV/ColorCorrectionHSL/src/ColorCorrectionAE.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ColorCorrectionHSL/src/ColorCorrectionAE.cpp
@@ -65,13 +65,13 @@ PF_Err ProcessImgInAE_16bits
 	PF_Err err = PF_Err_NONE;
 
 	/* scan control setting */
-	auto const& lwbType = params[COLOR_CORRECT_SPACE_POPUP]->u.pd.value;
+	auto const& lwbType   = params[COLOR_CORRECT_SPACE_POPUP]->u.pd.value;
 	auto const& hueCoarse = params[COLOR_CORRECT_HUE_COARSE_LEVEL]->u.ad.value;
-	auto const& hueFine = params[COLOR_HUE_FINE_LEVEL_SLIDER]->u.fs_d.value;
+	auto const& hueFine   = params[COLOR_HUE_FINE_LEVEL_SLIDER]->u.fs_d.value;
 	auto const& satCoarse = params[COLOR_SATURATION_COARSE_LEVEL_SLIDER]->u.sd.value;
-	auto const& satFine = params[COLOR_SATURATION_FINE_LEVEL_SLIDER]->u.fs_d.value;
+	auto const& satFine   = params[COLOR_SATURATION_FINE_LEVEL_SLIDER]->u.fs_d.value;
 	auto const& lwbCoarse = params[COLOR_LWIP_COARSE_LEVEL_SLIDER]->u.sd.value;
-	auto const& lwbFine = params[COLOR_LWIP_FINE_LEVEL_SLIDER]->u.fs_d.value;
+	auto const& lwbFine   = params[COLOR_LWIP_FINE_LEVEL_SLIDER]->u.fs_d.value;
 
 	float const& totalHue = normalize_hue_wheel(static_cast<float>(hueCoarse) / 65536.f + static_cast<float>(hueFine));
 	float const& totalSat = static_cast<float>(satCoarse) + static_cast<float>(satFine);
@@ -108,6 +108,89 @@ PF_Err ProcessImgInAE_16bits
 }
 
 
+PF_Err ProcessImgInAE_32bits
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    PF_Err err = PF_Err_NONE;
+
+    /* scan control setting */
+    auto const& lwbType   = params[COLOR_CORRECT_SPACE_POPUP]->u.pd.value;
+    auto const& hueCoarse = params[COLOR_CORRECT_HUE_COARSE_LEVEL]->u.ad.value;
+    auto const& hueFine   = params[COLOR_HUE_FINE_LEVEL_SLIDER]->u.fs_d.value;
+    auto const& satCoarse = params[COLOR_SATURATION_COARSE_LEVEL_SLIDER]->u.sd.value;
+    auto const& satFine   = params[COLOR_SATURATION_FINE_LEVEL_SLIDER]->u.fs_d.value;
+    auto const& lwbCoarse = params[COLOR_LWIP_COARSE_LEVEL_SLIDER]->u.sd.value;
+    auto const& lwbFine   = params[COLOR_LWIP_FINE_LEVEL_SLIDER]->u.fs_d.value;
+
+    float const& totalHue = normalize_hue_wheel(static_cast<float>(hueCoarse) / 65536.f + static_cast<float>(hueFine));
+    float const& totalSat = static_cast<float>(satCoarse) + static_cast<float>(satFine);
+    float const& totalLwb = static_cast<float>(lwbCoarse) + static_cast<float>(lwbFine);
+
+    eCOLOR_SPACE_TYPE const& colorSpaceType = static_cast<eCOLOR_SPACE_TYPE const>(lwbType - 1);
+
+    switch (colorSpaceType)
+    {
+        case COLOR_SPACE_HSL:
+            err = prProcessImage_ARGB_4444_32f_HSL(in_data, out_data, params, output, totalHue, totalSat, totalLwb);
+        break;
+        
+        case COLOR_SPACE_HSV:
+            err = prProcessImage_ARGB_4444_32f_HSV(in_data, out_data, params, output, totalHue, totalSat, totalLwb);
+        break;
+        
+        case COLOR_SPACE_HSI:
+            err = prProcessImage_ARGB_4444_32f_HSI(in_data, out_data, params, output, totalHue, totalSat, totalLwb);
+        break;
+        
+        case COLOR_SPACE_HSP:
+            err = prProcessImage_ARGB_4444_32f_HSP(in_data, out_data, params, output, totalHue, totalSat, totalLwb);
+        break;
+        
+        case COLOR_SPACE_HSLuv:
+            err = prProcessImage_ARGB_4444_32f_HSLuv(in_data, out_data, params, output, totalHue, totalSat, totalLwb);
+        break;
+        
+        case COLOR_SPACE_HPLuv:
+            err = prProcessImage_ARGB_4444_32f_HPLuv(in_data, out_data, params, output, totalHue, totalSat, totalLwb);
+        break;
+        
+        default:
+            err = PF_Err_INVALID_INDEX;
+        break;
+    }
+
+    return err;
+}
+
+
+inline PF_Err ProcessImgInAE_DeepWorld
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    PF_Err	err = PF_Err_NONE;
+    PF_PixelFormat format = PF_PixelFormat_INVALID;
+    AEFX_SuiteScoper<PF_WorldSuite2> wsP = AEFX_SuiteScoper<PF_WorldSuite2>(in_data, kPFWorldSuite, kPFWorldSuiteVersion2, out_data);
+    if (PF_Err_NONE == wsP->PF_GetPixelFormat(reinterpret_cast<PF_EffectWorld* __restrict>(&params[COLOR_CORRECT_INPUT]->u.ld), &format))
+    {
+        err = (format == PF_PixelFormat_ARGB128 ?
+            ProcessImgInAE_32bits(in_data, out_data, params, output) : ProcessImgInAE_16bits(in_data, out_data, params, output));
+    }
+    else
+        err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
+
+    return err;
+}
+
+
 PF_Err
 ProcessImgInAE
 (
@@ -118,6 +201,6 @@ ProcessImgInAE
 ) noexcept
 {
 	return (PF_WORLD_IS_DEEP(output) ?
-		ProcessImgInAE_16bits(in_data, out_data, params, output) :
-		ProcessImgInAE_8bits(in_data, out_data, params, output));
+        ProcessImgInAE_DeepWorld (in_data, out_data, params, output) :
+		ProcessImgInAE_8bits (in_data, out_data, params, output));
 }
--- AFTEREFFECT_PLUGIN.DEV/ImageGeometry/src/ImageGeometryAE.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageGeometry/src/ImageGeometryAE.cpp
@@ -1,4 +1,5 @@
 #include "ImageGeometry.hpp"
+#include "ImageGeometryEnum.hpp"
 
 
 PF_Err ImageGeometry_InAE_8bits
@@ -21,6 +22,40 @@ PF_Err ImageGeometry_InAE_16bits
 ) noexcept
 {
 	return PF_Err_NONE;
+}
+
+PF_Err ImageGeometry_InAE_32bits
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    return PF_Err_NONE;
+}
+
+
+inline PF_Err ImageSharpening_InAE_DeepWorld
+(
+    PF_InData*   in_data,
+    PF_OutData*  out_data,
+    PF_ParamDef* params[],
+    PF_LayerDef* output
+) noexcept
+{
+    PF_Err	err = PF_Err_NONE;
+    PF_PixelFormat format = PF_PixelFormat_INVALID;
+    AEFX_SuiteScoper<PF_WorldSuite2> wsP = AEFX_SuiteScoper<PF_WorldSuite2>(in_data, kPFWorldSuite, kPFWorldSuiteVersion2, out_data);
+    if (PF_Err_NONE == wsP->PF_GetPixelFormat(reinterpret_cast<PF_EffectWorld* __restrict>(&params[eIMAGE_GEOMETRY_INPUT]->u.ld), &format))
+    {
+        err = (format == PF_PixelFormat_ARGB128 ?
+            ImageGeometry_InAE_32bits(in_data, out_data, params, output) : ImageGeometry_InAE_16bits(in_data, out_data, params, output));
+    }
+    else
+        err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
+
+    return err;
 }
 
 
