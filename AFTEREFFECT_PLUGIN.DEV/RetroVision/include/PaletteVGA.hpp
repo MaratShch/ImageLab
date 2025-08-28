9c650b3472365b66dc6b10e717e0a916d89e0cb4
Marat Shchuchinsky <maratshch@gmail.com>
2025-08-28 22:48:58 +0300

    continue with Palette - add VGA  standard 16 colors palette

--- AFTEREFFECT_PLUGIN.DEV/RetroVision/include/PaletteVGA.hpp
+++ AFTEREFFECT_PLUGIN.DEV/RetroVision/include/PaletteVGA.hpp
@@ -4,5 +4,26 @@
 #include "PaletteEntry.hpp"
 #include <array>
 
+// EGA standard palette
+constexpr std::array<PEntry<uint8_t>, 16> VGA_Standard16_u8 =
+{{
+    {   0,   0,   0 },
+    {   0,   0, 170 },
+    {   0, 170,   0 },
+    {   0, 170, 170 },
+    { 170,   0,   0 },
+    { 170,   0, 170 },
+    { 170,  85,   0 },
+    { 170, 170, 170 },
+    {  85,  85,  85 },
+    {  85,  85, 255 },
+    {  85, 255,  85 },
+    {  85, 255, 255 },
+    { 255,  85,  85 },
+    { 255,  85, 255 },
+    { 255, 255,  85 },
+    { 255, 255, 255 }
+}};
+
 
 #endif // __IMAGE_LAB_RETRO_VISION_PALETTE_VGA_VALUES__
