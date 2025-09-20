d6ebb0d3259c2352289747daad845d473cc664e2
Marat Shchuchinsky <maratshch@gmail.com>
2025-09-20 21:13:46 +0300

    dev. tmp commit

--- AFTEREFFECT_PLUGIN.DEV/RetroVision/src/RetroVisionAlgorithm.cpp
+++ AFTEREFFECT_PLUGIN.DEV/RetroVision/src/RetroVisionAlgorithm.cpp
@@ -288,102 +288,13 @@ void RetroResolution_Simulation
         default:
         {
             const PEntry<float> whiteLevel = HERCULES_White_ColorF32[UnderlyingType(controlParams.white_color_hercules)];
-            Hercules_Simulation (input, output, sizeX, sizeY, controlParams.hercules_threshold, whiteLevel);
+            const float threshold = static_cast<float>(controlParams.hercules_threshold) / 255.f;
+            Hercules_Simulation (input, output, sizeX, sizeY, threshold, whiteLevel);
         }
         break;
     }
 
     // Simulate CRT Artifacts
-
-
-#if 0
-    // check moitor type
-    const RetroMonitor monitor = static_cast<RetroMonitor>(params[UnderlyingType(RetroVision::eRETRO_VISION_DISPLAY)]->u.bd.value - 1);
-    switch (monitor)
-    {
-        case RetroMonitor::eRETRO_BITMAP_CGA:
-        {
-            const PaletteCGA palette = static_cast<PaletteCGA>(params[UnderlyingType(RetroVision::eRETRO_VISION_CGA_PALETTE)]->u.bd.value - 1);
-            const A_long intencity = params[UnderlyingType(RetroVision::eRETRO_VISION_CGA_INTTENCITY_BIT)]->u.bd.value;
-            switch (palette)
-            {
-                case PaletteCGA::eRETRO_PALETTE_CGA1:
-                {
-                    const CGA_PaletteF32& paletteCGA1 = (0 == intencity ? CGA0_f32 : CGA0i_f32);
-                    CGA_Simulation (input, output, sizeX, sizeY, paletteCGA1);
-                }
-                break;
-                case PaletteCGA::eRETRO_PALETTE_CGA2:
-                {
-                    const CGA_PaletteF32& paletteCGA2 = (0 == intencity ? CGA1_f32 : CGA1i_f32);
-                    CGA_Simulation (input, output, sizeX, sizeY, paletteCGA2);
-                }
-                break;
-            }
-        }
-        break;
-
-        case RetroMonitor::eRETRO_BITMAP_EGA:
-        {
-            const PaletteEGA palette = static_cast<PaletteEGA>(params[UnderlyingType(RetroVision::eRETRO_VISION_EGA_PALETTE)]->u.bd.value - 1);
-            switch (palette)
-            {
-                case PaletteEGA::eRETRO_PALETTE_EGA_STANDARD:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_Standard_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_KING_QUESTS:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_KQ3_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_KYRANDIA:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_Kyrandia_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_THEXDER:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_Thexder_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_DUNE:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_Dune_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_DOOM:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_Doom_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_METAL_MUTANT:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_MetalMutant_u8);
-                break;
-
-                case PaletteEGA::eRETRO_PALETTE_EGA_WOLFENSTEIN:
-                default:
-                    EGA_Simulation (input, output, sizeX, sizeY, EGA_Wolfenstein_u8);
-                break;
-            }
-        }
-        break;
-
-        case RetroMonitor::eRETRO_BITMAP_VGA:
-        {
-            const PaletteVGA palette = static_cast<PaletteVGA>(params[UnderlyingType(RetroVision::eRETRO_VISION_VGA_PALETTE)]->u.bd.value - 1);
-            if (PaletteVGA::eRETRO_PALETTE_VGA_16_BITS == palette)
-                VGA16_Simulation  (input, output, sizeX, sizeY, VGA_Standard16_u8);
-            else
-                VGA256_Simulation (input, output, sizeX, sizeY, VGA_Standard256_u8);
-        }
-        break;
-
-        case RetroMonitor::eRETRO_BITMAP_HERCULES:
-        default:
-        {
-            const int32_t bwThreshold = params[UnderlyingType(RetroVision::eRETRO_VISION_HERCULES_THRESHOLD)]->u.sd.value;
-            const float binThreshold = bwThreshold / 255.f;
-            Hercules_Simulation (input, output, sizeX, sizeY, binThreshold);
-        }
-        break;
-    }
-#endif
 
     return;
 }
