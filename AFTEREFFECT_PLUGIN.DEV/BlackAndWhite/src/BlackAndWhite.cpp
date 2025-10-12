fb8ccd6944ad8b7cb1998f38cb94b6296b452b54
Marat Shchuchinsky <maratshch@gmail.com>
2025-10-12 21:32:14 +0300

    add missed SmartRender to BlackAndWhite

--- AFTEREFFECT_PLUGIN.DEV/BlackAndWhite/src/BlackAndWhite.cpp
+++ AFTEREFFECT_PLUGIN.DEV/BlackAndWhite/src/BlackAndWhite.cpp
@@ -1,5 +1,6 @@
 #include "BlackAndWhiteProc.hpp"
 #include "PrSDKAESupport.h"
+#include "CommonSmartRender.hpp"
 
 static PF_Err
 About(
@@ -119,24 +120,154 @@ Render(
 
 
 static PF_Err
-SmartPreRender(
-	PF_InData		*in_data,
-	PF_OutData		*out_data,
-	PF_ParamDef		*params[],
-	PF_LayerDef		*output)
+PreRender
+(
+    PF_InData			*in_data,
+    PF_OutData			*out_data,
+    PF_PreRenderExtra	*extra
+)
 {
-	return PF_Err_NONE;
+    A_long filterAdvancedAlgoParam = 0;
+    PF_Err err = PF_Err_NONE;
+
+    AEFX_SuiteScoper<PF_HandleSuite1> handleSuite = AEFX_SuiteScoper<PF_HandleSuite1>(in_data, kPFHandleSuite, kPFHandleSuiteVersion1, out_data);
+    PF_Handle paramsHandler = handleSuite->host_new_handle(sizeof(filterAdvancedAlgoParam));
+    if (nullptr != paramsHandler)
+    {
+        A_long* paramsP = reinterpret_cast<A_long*>(handleSuite->host_lock_handle(paramsHandler));
+        if (nullptr != paramsP)
+        {
+            extra->output->pre_render_data = paramsHandler;
+
+            PF_ParamDef	advancdeAlgo{};
+            const PF_Err errParam1 = PF_CHECKOUT_PARAM(in_data, IMAGE_BW_ADVANCED_ALGO, in_data->current_time, in_data->time_step, in_data->time_scale, &advancdeAlgo);
+
+            *paramsP = (PF_Err_NONE == errParam1 ? 0x1 & advancdeAlgo.u.bd.value : 0x0);
+
+            PF_RenderRequest req = extra->input->output_request;
+            PF_CheckoutResult in_result{};
+
+            ERR(extra->cb->checkout_layer
+            (in_data->effect_ref, IMAGE_BW_FILTER_INPUT, IMAGE_BW_FILTER_INPUT, &req, in_data->current_time, in_data->time_step, in_data->time_scale, &in_result));
+
+            UnionLRect(&in_result.result_rect, &extra->output->result_rect);
+            UnionLRect(&in_result.max_result_rect, &extra->output->max_result_rect);
+            handleSuite->host_unlock_handle(paramsHandler);
+
+        } // if (nullptr != paramsStrP)
+        else
+            err = PF_Err_INTERNAL_STRUCT_DAMAGED;
+    } // if (nullptr != paramsHandler)
+    else
+        err = PF_Err_OUT_OF_MEMORY;
+
+    return err;
 }
 
 
 static PF_Err
-SmartRender(
-	PF_InData		*in_data,
-	PF_OutData		*out_data,
-	PF_ParamDef		*params[],
-	PF_LayerDef		*output)
+SmartRender
+(
+    PF_InData				*in_data,
+    PF_OutData				*out_data,
+    PF_SmartRenderExtra		*extraP
+)
 {
-	return PF_Err_NONE;
+    PF_EffectWorld* input_worldP = nullptr;
+    PF_EffectWorld* output_worldP = nullptr;
+    PF_Err	err = PF_Err_NONE;
+
+    AEFX_SuiteScoper<PF_HandleSuite1> handleSuite = AEFX_SuiteScoper<PF_HandleSuite1>(in_data, kPFHandleSuite, kPFHandleSuiteVersion1, out_data);
+    const A_long* pFilterStrParams = reinterpret_cast<const A_long*>(handleSuite->host_lock_handle(reinterpret_cast<PF_Handle>(extraP->input->pre_render_data)));
+
+    if (nullptr != pFilterStrParams)
+    {
+        ERR((extraP->cb->checkout_layer_pixels(in_data->effect_ref, IMAGE_BW_FILTER_INPUT, &input_worldP)));
+        ERR(extraP->cb->checkout_output(in_data->effect_ref, &output_worldP));
+
+        if (nullptr != input_worldP && nullptr != output_worldP)
+        {
+            const A_long sizeX = input_worldP->width;
+            const A_long sizeY = input_worldP->height;
+            const A_long srcRowBytes = input_worldP->rowbytes;  // Get input buffer pitch in bytes
+            const A_long dstRowBytes = output_worldP->rowbytes; // Get output buffer pitch in bytes
+
+            AEFX_SuiteScoper<PF_WorldSuite2> wsP = AEFX_SuiteScoper<PF_WorldSuite2>(in_data, kPFWorldSuite, kPFWorldSuiteVersion2, out_data);
+
+            const A_long advancedAlgo = *pFilterStrParams & 0x1;
+
+            PF_PixelFormat format = PF_PixelFormat_INVALID;
+            if (PF_Err_NONE == wsP->PF_GetPixelFormat(input_worldP, &format))
+            {
+                switch (format)
+                {
+                    case PF_PixelFormat_ARGB128:
+                    {
+                        constexpr PF_Pixel_ARGB_32f white{ f32_value_white , f32_value_white , f32_value_white , f32_value_white };
+                        constexpr PF_Pixel_ARGB_32f black{ f32_value_black , f32_value_black , f32_value_black , f32_value_black };
+                        const A_long srcPitch = srcRowBytes / static_cast<A_long>(PF_Pixel_ARGB_32f_size);
+                        const A_long dstPitch = dstRowBytes / static_cast<A_long>(PF_Pixel_ARGB_32f_size);
+
+                        const PF_Pixel_ARGB_32f* __restrict input_pixels  = reinterpret_cast<const PF_Pixel_ARGB_32f* __restrict>(input_worldP->data);
+                              PF_Pixel_ARGB_32f* __restrict output_pixels = reinterpret_cast<      PF_Pixel_ARGB_32f* __restrict>(output_worldP->data);
+
+                        if (0 == advancedAlgo)
+                            ProcessImage (input_pixels, output_pixels, sizeX, sizeY, srcPitch, dstPitch, 0);
+                        else
+                            ProcessImageAdvanced (input_pixels, output_pixels, sizeX, sizeY, srcPitch, dstPitch);
+                    }
+                    break;
+
+                    case PF_PixelFormat_ARGB64:
+                    {
+                        constexpr PF_Pixel_ARGB_16u white{ u16_value_white , u16_value_white , u16_value_white , u16_value_white };
+                        constexpr PF_Pixel_ARGB_16u black{ u16_value_black , u16_value_black , u16_value_black , u16_value_black };
+                        const A_long srcPitch = srcRowBytes / static_cast<A_long>(PF_Pixel_ARGB_16u_size);
+                        const A_long dstPitch = dstRowBytes / static_cast<A_long>(PF_Pixel_ARGB_16u_size);
+
+                        const PF_Pixel_ARGB_16u* __restrict input_pixels  = reinterpret_cast<const PF_Pixel_ARGB_16u* __restrict>(input_worldP->data);
+                              PF_Pixel_ARGB_16u* __restrict output_pixels = reinterpret_cast<      PF_Pixel_ARGB_16u* __restrict>(output_worldP->data);
+
+                        if (0 == advancedAlgo)
+                            ProcessImage (input_pixels, output_pixels, sizeX, sizeY, srcPitch, dstPitch, 0);
+                        else
+                            ProcessImageAdvanced (input_pixels, output_pixels, sizeX, sizeY, srcPitch, dstPitch);
+                    }
+                    break;
+
+                    case PF_PixelFormat_ARGB32:
+                    {
+                        constexpr PF_Pixel_ARGB_8u white{ u8_value_white , u8_value_white , u8_value_white , u8_value_white };
+                        constexpr PF_Pixel_ARGB_8u black{ u8_value_black , u8_value_black , u8_value_black , u8_value_black };
+                        const A_long srcPitch = srcRowBytes / static_cast<A_long>(PF_Pixel_ARGB_8u_size);
+                        const A_long dstPitch = dstRowBytes / static_cast<A_long>(PF_Pixel_ARGB_8u_size);
+
+                        const PF_Pixel_ARGB_8u* __restrict input_pixels  = reinterpret_cast<const PF_Pixel_ARGB_8u* __restrict>(input_worldP->data);
+                              PF_Pixel_ARGB_8u* __restrict output_pixels = reinterpret_cast<      PF_Pixel_ARGB_8u* __restrict>(output_worldP->data);
+
+                        if (0 == advancedAlgo)
+                            ProcessImage (input_pixels, output_pixels, sizeX, sizeY, srcPitch, dstPitch, 0);
+                        else
+                            ProcessImageAdvanced (input_pixels, output_pixels, sizeX, sizeY, srcPitch, dstPitch);
+                    }
+                    break;
+
+                    default:
+                        err = PF_Err_BAD_CALLBACK_PARAM;
+                    break;
+                } // switch (format)
+
+            } // if (PF_Err_NONE == wsP->PF_GetPixelFormat(input_worldP, &format))
+
+        } // if (nullptr != input_worldP && nullptr != output_worldP)
+
+        handleSuite->host_unlock_handle(reinterpret_cast<PF_Handle>(extraP->input->pre_render_data));
+
+    } // if (nullptr != pFilterStrParams)
+    else
+        err = PF_Err_OUT_OF_MEMORY;
+
+    return err;
 }
 
 
@@ -181,13 +312,13 @@ EffectMain(
 				ERR(Render(in_data, out_data, params, output));
 			break;
 
-			case PF_Cmd_SMART_PRE_RENDER:
-				ERR(SmartPreRender(in_data, out_data, params, output));
-			break;
+            case PF_Cmd_SMART_PRE_RENDER:
+                ERR(PreRender(in_data, out_data, reinterpret_cast<PF_PreRenderExtra*>(extra)));
+            break;
 
-			case PF_Cmd_SMART_RENDER:
-				ERR(SmartRender(in_data, out_data, params, output));
-			break;
+            case PF_Cmd_SMART_RENDER:
+                ERR(SmartRender(in_data, out_data, reinterpret_cast<PF_SmartRenderExtra*>(extra)));
+            break;
 
 			default:
 			break;
