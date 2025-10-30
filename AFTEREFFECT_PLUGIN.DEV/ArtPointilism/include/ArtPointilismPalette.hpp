0aa79bdaa61fa600915e2ec500c554f35819d0ef
Marat Shchuchinsky <maratshch@gmail.com>
2025-10-30 21:08:08 +0200

    tmp

--- AFTEREFFECT_PLUGIN.DEV/ArtPointilism/include/ArtPointilismPalette.hpp
+++ AFTEREFFECT_PLUGIN.DEV/ArtPointilism/include/ArtPointilismPalette.hpp
@@ -1,5 +1,15 @@
 #ifndef __IMAGE_LAB_ART_POINTILISM_PALETTE_DEFINITION__
 #define __IMAGE_LAB_ART_POINTILISM_PALETTE_DEFINITION__
 
+#include <type_traits>
+
+template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value>::type>
+struct PEntry
+{
+    T r;
+    T g;
+    T b;
+};
+
 
 #endif // __IMAGE_LAB_ART_POINTILISM_PALETTE_DEFINITION__
