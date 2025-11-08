6d281f6c0e9714a83c12beef6943cd2c48746bc3
Marat Shchuchinsky <maratshch@gmail.com>
2025-11-08 19:15:15 +0200

    add to Art::Pointilism effect color conversion functions for convert to CIELab color space and back to native format from CIELab

--- AFTEREFFECT_PLUGIN.DEV/ArtPointillism/ArtPointillism.r
+++ AFTEREFFECT_PLUGIN.DEV/ArtPointillism/ArtPointillism.r
@@ -13,7 +13,7 @@ resource 'PiPL' (16000) {
 		},
 		/* [2] */
 		Name {
-			"Art::Pointilism"
+			"Art::Pointillism"
 		},
 		/* [3] */
 		Category {
@@ -60,7 +60,7 @@ resource 'PiPL' (16000) {
 		},
 		/* [11] */
 		AE_Effect_Match_Name {
-			"ImageLab2 Art::Pointilism"
+			"ImageLab2 Art::Pointillism"
 		},
 		/* [12] */
 		AE_Reserved_Info {
--- AFTEREFFECT_PLUGIN.DEV/ArtPointillism/include/ArtPointillism.hpp
+++ AFTEREFFECT_PLUGIN.DEV/ArtPointillism/include/ArtPointillism.hpp
@@ -4,7 +4,7 @@
 #include "CommonAdobeAE.hpp"
 
 
-constexpr char strName[] = "LrPreset";
+constexpr char strName[] = "Art Pointillism";
 constexpr char strCopyright[] = "\n2019-2025. ImageLab2 Copyright(c).\rArt Pointillism plugin.";
 constexpr int ArtPointillism_VersionMajor = IMAGE_LAB_AE_PLUGIN_VERSION_MAJOR;
 constexpr int ArtPointillism_VersionMinor = IMAGE_LAB_AE_PLUGIN_VERSION_MINOR;
