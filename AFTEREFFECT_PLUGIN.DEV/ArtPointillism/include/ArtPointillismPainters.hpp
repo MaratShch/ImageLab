8ffe93aa1a164e89bdcee105a57738e378e020be
Marat Shchuchinsky <maratshch@gmail.com>
2026-01-03 16:37:20 +0200

    little re-org in project folders infrastructure in ArtPointillism

--- AFTEREFFECT_PLUGIN.DEV/ArtPointillism/include/ArtPointillismPainters.hpp
+++ AFTEREFFECT_PLUGIN.DEV/ArtPointillism/include/ArtPointillismPainters.hpp
@@ -1,14 +1,14 @@
 #ifndef __IMAGE_LAB_ART_POINTILISM_PAINTERS_TECHNISC_SPECIFIC_DEFINITIONS__
 #define __IMAGE_LAB_ART_POINTILISM_PAINTERS_TECHNISC_SPECIFIC_DEFINITIONS__
 
-#include "ArtPointillismCross.hpp"
-#include "ArtPointillismLuce.hpp"
-#include "ArtPointillismMatisse.hpp"
-#include "ArtPointillismPissaro.hpp"
-#include "ArtPointillismRysselberghe.hpp"
-#include "ArtPointillismSeurat.hpp"
-#include "ArtPointillismSignac.hpp"
-#include "ArtPointillismVanGogh.hpp"
+#include "PainterPalette/ArtPointillismCross.hpp"
+#include "PainterPalette/ArtPointillismLuce.hpp"
+#include "PainterPalette/ArtPointillismMatisse.hpp"
+#include "PainterPalette/ArtPointillismPissaro.hpp"
+#include "PainterPalette/ArtPointillismRysselberghe.hpp"
+#include "PainterPalette/ArtPointillismSeurat.hpp"
+#include "PainterPalette/ArtPointillismSignac.hpp"
+#include "PainterPalette/ArtPointillismVanGogh.hpp"
 
 
 template <typename T>
