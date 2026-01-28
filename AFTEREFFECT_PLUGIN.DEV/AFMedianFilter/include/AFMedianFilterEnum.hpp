7960f77eadcf505f64d5d874fd34ea689f3ce5e3
Marat Shchuchinsky <maratshch@gmail.com>
2026-01-28 23:06:07 +0200

    tmp

--- AFTEREFFECT_PLUGIN.DEV/AFMedianFilter/include/AFMedianFilterEnum.hpp
+++ AFTEREFFECT_PLUGIN.DEV/AFMedianFilter/include/AFMedianFilterEnum.hpp
@@ -10,6 +10,8 @@ enum class AFMF : int32_t
     eIMAGE_AFMEDIAN_TOTAL_CONTROLS
 };
 
+constexpr int32_t kerenlSizeMin = 3;
+constexpr int32_t kernelSizeMax = 31;
 
 
 #endif // __IMAGE_LAB_IMAGE_GEOMETRY_ENUMERATORS_FILTER__
