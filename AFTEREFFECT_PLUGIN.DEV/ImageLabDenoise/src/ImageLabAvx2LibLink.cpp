84d8c83dc9bafeb029efe851d859cde11263e90f
Marat Shchuchinsky <maratshch@gmail.com>
2026-02-21 21:53:04 +0200

    start implement algorithm for ImageDenoise

--- AFTEREFFECT_PLUGIN.DEV/ImageLabDenoise/src/ImageLabAvx2LibLink.cpp
+++
@@ -1,8 +0,0 @@
-
-// static link with AVX2 library
-#ifdef _DEBUG
-#pragma comment(lib, "..\\BUILD.OUT\\Debug\\Avx2ProcLib.lib")
-#else 
-#pragma comment(lib, "..\\BUILD.OUT\\Release\\Avx2ProcLib.lib")
-#endif
-
--- AFTEREFFECT_PLUGIN.DEV/ImageLabDenoise/src/ImageLabDenoisePR.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ImageLabDenoise/src/ImageLabDenoisePR.cpp
@@ -5,53 +5,135 @@
 
 PF_Err ProcessImgInPR
 (
-	PF_InData*   __restrict in_data,
-	PF_OutData*  __restrict out_data,
-	PF_ParamDef* __restrict params[],
-	PF_LayerDef* __restrict output
-) 
+    PF_InData*   __restrict in_data,
+    PF_OutData*  __restrict out_data,
+    PF_ParamDef* __restrict params[],
+    PF_LayerDef* __restrict output
+)
 {
-	PF_Err err{ PF_Err_NONE };
-	PF_Err errFormat{ PF_Err_INVALID_INDEX };
-	PrPixelFormat destinationPixelFormat{ PrPixelFormat_Invalid };
+    PF_Err err = PF_Err_NONE;
+    PF_Err errFormat = PF_Err_INVALID_INDEX;
+    PrPixelFormat destinationPixelFormat = PrPixelFormat_Invalid;
 
-	/* This plugin called frop PR - check video fomat */
-	auto const pixelFormatSuite{ AEFX_SuiteScoper<PF_PixelFormatSuite1>(in_data, kPFPixelFormatSuite, kPFPixelFormatSuiteVersion1, out_data) };
+    const PF_LayerDef* pfLayer = reinterpret_cast<const PF_LayerDef*>(&params[UnderlyingType(DenoiseControl::eIMAGE_LAB_DENOISE_INPUT)]->u.ld);
+    const A_long sizeY = pfLayer->extent_hint.bottom - pfLayer->extent_hint.top;
+    const A_long sizeX = pfLayer->extent_hint.right - pfLayer->extent_hint.left;
 
-	if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat)))
-	{
-		switch (destinationPixelFormat)
-		{
-			case PrPixelFormat_BGRA_4444_8u:
-			break;
+//    MemHandler algoMemHandler = alloc_memory_buffers(sizeX, sizeY);
+//    if (true == mem_handler_valid(algoMemHandler))
+//    {
+        // This plugin called frop PR - check video fomat
+        auto const pixelFormatSuite{ AEFX_SuiteScoper<PF_PixelFormatSuite1>(in_data, kPFPixelFormatSuite, kPFPixelFormatSuiteVersion1, out_data) };
 
-			case PrPixelFormat_BGRA_4444_16u:
-			break;
+        if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat)))
+        {
+//            const PontillismControls algoControls = GetControlParametersStruct(params);
 
-			case PrPixelFormat_BGRA_4444_32f:
-			break;
+            switch (destinationPixelFormat)
+            {
+                case PrPixelFormat_BGRA_4444_8u:
+                {
+                    const PF_Pixel_BGRA_8u* __restrict localSrc = reinterpret_cast<const PF_Pixel_BGRA_8u* __restrict>(pfLayer->data);
+                    PF_Pixel_BGRA_8u* __restrict localDst = reinterpret_cast<      PF_Pixel_BGRA_8u* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_BGRA_8u_size);
 
-			case PrPixelFormat_VUYA_4444_8u_709:
-			case PrPixelFormat_VUYA_4444_8u:
-			break;
+                    // convert to semi-planar CieLAB color space
+                }
+                break;
 
-			case PrPixelFormat_VUYA_4444_32f_709:
-			case PrPixelFormat_VUYA_4444_32f:
-			break;
+                case PrPixelFormat_BGRA_4444_16u:
+                {
+                    const PF_Pixel_BGRA_16u* __restrict localSrc = reinterpret_cast<const PF_Pixel_BGRA_16u* __restrict>(pfLayer->data);
+                    PF_Pixel_BGRA_16u* __restrict localDst = reinterpret_cast<      PF_Pixel_BGRA_16u* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_BGRA_16u_size);
 
-			case PrPixelFormat_RGB_444_10u:
-			break;
+                }
+                break;
 
-			default:
-			break;
-		} /* switch (destinationPixelFormat) */
+                case PrPixelFormat_BGRA_4444_32f:
+                {
+                    const PF_Pixel_BGRA_32f* __restrict localSrc = reinterpret_cast<const PF_Pixel_BGRA_32f* __restrict>(pfLayer->data);
+                    PF_Pixel_BGRA_32f* __restrict localDst = reinterpret_cast<      PF_Pixel_BGRA_32f* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_BGRA_32f_size);
 
-	} /* if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat))) */
-	else
-	{
-		/* error in determine pixel format */
-		err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
-	}
+                }
+                break;
 
-	return err;
+                case PrPixelFormat_VUYA_4444_8u_709:
+                case PrPixelFormat_VUYA_4444_8u:
+                {
+                    const PF_Pixel_VUYA_8u* __restrict localSrc = reinterpret_cast<const PF_Pixel_VUYA_8u* __restrict>(pfLayer->data);
+                    PF_Pixel_VUYA_8u* __restrict localDst = reinterpret_cast<      PF_Pixel_VUYA_8u* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_VUYA_8u_size);
+                    const bool isBT709 = (PrPixelFormat_VUYA_4444_8u_709 == destinationPixelFormat);
+
+                }
+                break;
+
+                case PrPixelFormat_VUYA_4444_32f_709:
+                case PrPixelFormat_VUYA_4444_32f:
+                {
+                    const PF_Pixel_VUYA_32f* __restrict localSrc = reinterpret_cast<const PF_Pixel_VUYA_32f* __restrict>(pfLayer->data);
+                    PF_Pixel_VUYA_32f* __restrict localDst = reinterpret_cast<      PF_Pixel_VUYA_32f* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_VUYA_32f_size);
+                    const bool isBT709 = (PrPixelFormat_VUYA_4444_32f_709 == destinationPixelFormat);
+
+                }
+                break;
+
+                case PrPixelFormat_RGB_444_10u:
+                {
+                    const PF_Pixel_RGB_10u* __restrict localSrc = reinterpret_cast<const PF_Pixel_RGB_10u* __restrict>(pfLayer->data);
+                    PF_Pixel_RGB_10u* __restrict localDst = reinterpret_cast<      PF_Pixel_RGB_10u* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_RGB_10u_size);
+
+                }
+                break;
+
+                case PrPixelFormat_ARGB_4444_8u:
+                {
+                    const PF_Pixel_ARGB_8u* __restrict localSrc = reinterpret_cast<const PF_Pixel_ARGB_8u* __restrict>(pfLayer->data);
+                    PF_Pixel_ARGB_8u* __restrict localDst = reinterpret_cast<      PF_Pixel_ARGB_8u* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_ARGB_8u_size);
+
+                }
+                break;
+
+                case PrPixelFormat_ARGB_4444_16u:
+                {
+                    const PF_Pixel_ARGB_16u* __restrict localSrc = reinterpret_cast<const PF_Pixel_ARGB_16u* __restrict>(pfLayer->data);
+                    PF_Pixel_ARGB_16u* __restrict localDst = reinterpret_cast<      PF_Pixel_ARGB_16u* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_ARGB_16u_size);
+
+                }
+                break;
+
+                case PrPixelFormat_ARGB_4444_32f:
+                {
+                    const PF_Pixel_ARGB_32f* __restrict localSrc = reinterpret_cast<const PF_Pixel_ARGB_32f* __restrict>(pfLayer->data);
+                    PF_Pixel_ARGB_32f* __restrict localDst = reinterpret_cast<      PF_Pixel_ARGB_32f* __restrict>(output->data);
+                    const A_long linePitch = pfLayer->rowbytes / static_cast<A_long>(PF_Pixel_BGRA_32f_size);
+
+                }
+                break;
+
+                default:
+                    err = PF_Err_INTERNAL_STRUCT_DAMAGED;
+                break;
+            } // switch (destinationPixelFormat)
+
+        } // if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat)))
+        else
+        {
+            // error in determine pixel format
+            err = PF_Err_UNRECOGNIZED_PARAM_TYPE;
+        }
+
+//        free_memory_buffers(algoMemHandler);
+
+//    } // if (true == mem_handler_valid (algoMemHandler))
+//    else
+//        err = PF_Err_OUT_OF_MEMORY;
+
+    return err;
 }
