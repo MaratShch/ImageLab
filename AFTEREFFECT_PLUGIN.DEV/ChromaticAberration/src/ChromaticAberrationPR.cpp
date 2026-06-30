4d5f0af53fabefd25e63cfb9d5d18d4df33b007f
Marat Shchuchinsky <maratshch@gmail.com>
2026-06-30 09:41:03 +0300

    dev. commit

--- AFTEREFFECT_PLUGIN.DEV/ChromaticAberration/src/ChromaticAberrationPR.cpp
+++ AFTEREFFECT_PLUGIN.DEV/ChromaticAberration/src/ChromaticAberrationPR.cpp
@@ -19,30 +19,49 @@ PF_Err ProcessImgInPR
 
 	if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat)))
 	{
-		switch (destinationPixelFormat)
-		{
-			case PrPixelFormat_BGRA_4444_8u:
-			break;
+        switch (destinationPixelFormat)
+        {
+            case PrPixelFormat_BGRA_4444_8u:
+            case PrPixelFormat_BGRA_4444_16u:
+            case PrPixelFormat_BGRA_4444_32f:
+            case PrPixelFormat_BGRA_4444_32f_Linear:
+            case PrPixelFormat_BGRP_4444_8u:
+            case PrPixelFormat_BGRP_4444_16u:
+            case PrPixelFormat_BGRP_4444_32f:
+            case PrPixelFormat_BGRP_4444_32f_Linear:
+            case PrPixelFormat_BGRX_4444_8u:
+            case PrPixelFormat_BGRX_4444_16u:
+            case PrPixelFormat_BGRX_4444_32f:
+            case PrPixelFormat_BGRX_4444_32f_Linear:
+            case PrPixelFormat_VUYA_4444_8u_709:
+            case PrPixelFormat_VUYA_4444_8u:
+            case PrPixelFormat_VUYA_4444_32f_709:
+            case PrPixelFormat_VUYA_4444_32f:
+            case PrPixelFormat_VUYP_4444_8u_709:
+            case PrPixelFormat_VUYP_4444_8u:
+            case PrPixelFormat_VUYP_4444_32f_709:
+            case PrPixelFormat_VUYP_4444_32f:
+            case PrPixelFormat_VUYX_4444_8u_709:
+            case PrPixelFormat_VUYX_4444_8u:
+            case PrPixelFormat_VUYX_4444_32f_709:
+            case PrPixelFormat_VUYX_4444_32f:
+            case PrPixelFormat_ARGB_4444_8u:
+            case PrPixelFormat_ARGB_4444_16u:
+            case PrPixelFormat_ARGB_4444_32f:
+            case PrPixelFormat_PRGB_4444_32f:
+            case PrPixelFormat_XRGB_4444_32f:
+            case PrPixelFormat_ARGB_4444_32f_Linear:
+            case PrPixelFormat_PRGB_4444_32f_Linear:
+            case PrPixelFormat_XRGB_4444_32f_Linear:
 
-			case PrPixelFormat_BGRA_4444_16u:
-			break;
+            case PrPixelFormat_RGB_444_10u:
+            case PrPixelFormat_RGB_444_12u_PQ_709:
+            case PrPixelFormat_RGB_444_12u_PQ_P3:
+            case PrPixelFormat_RGB_444_12u_PQ_2020:
 
-			case PrPixelFormat_BGRA_4444_32f:
-			break;
-
-			case PrPixelFormat_VUYA_4444_8u_709:
-			case PrPixelFormat_VUYA_4444_8u:
-			break;
-
-			case PrPixelFormat_VUYA_4444_32f_709:
-			case PrPixelFormat_VUYA_4444_32f:
-			break;
-
-			case PrPixelFormat_RGB_444_10u:
-			break;
-
-			default:
-			break;
+            default:
+                err = PF_Err_INVALID_INDEX;
+            break;
 		} /* switch (destinationPixelFormat) */
 
 	} /* if (PF_Err_NONE == (errFormat = pixelFormatSuite->GetPixelFormat(output, &destinationPixelFormat))) */
