bcf4ecc9b2c4691ab8240bbc1af645d0d1a3b627
Marat Shchuchinsky <maratshch@gmail.com>
2026-07-07 21:31:49 +0300

    add precomputed CCT LUT for ColorEngine

---
+++ AFTEREFFECT_PLUGIN.DEV/ColorEngine/include/CCTLut/CCTLut.hpp
@@ -0,0 +1,12 @@
+#ifndef __IMAGE_LAB2_COLOR_ENGINE_CCT_LUT__
+#define __IMAGE_LAB2_COLOR_ENGINE_CCT_LUT__
+
+#if __cplusplus >= 202002L
+ #include "cpp20/CCT_LUT_CIE_1931_2DEG.hpp"
+ #include "cpp20/CCT_LUT_CIE_1964_10DEG.hpp"
+#else
+ #include "cpp14/CCT_LUT_CIE_1931_2DEG.hpp"
+ #include "cpp14/CCT_LUT_CIE_1964_10DEG.hpp"
+#endif
+
+#endif // __IMAGE_LAB2_COLOR_ENGINE_CCT_LUT__
