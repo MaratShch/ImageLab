5a46fac0da4e4acebbb11c245d3e363d8e7499af
Marat Shchuchinsky <maratshch@gmail.com>
2026-09-21 00:16:40 +0300

    tmp

---
+++ AFTEREFFECT_PLUGIN.DEV/FilmSimulation/include/AlgoControlAdobe.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Common.hpp"
+#include "CompileTimeUtils.hpp"
+#include "AlgoControlEnums.hpp"
+#include "AlgoAdobeControlEnums.hpp"
+#include "film_params_mask.hpp"
+
+
+template <typename T>
+inline const T get_list_box_value (PF_ParamDef* params[], const FilmSimulationCtrl idx) noexcept
+{
+    return static_cast<T>(params[UnderlyingType(idx)]->u.pd.value - 1);
+}
+
+inline const int32_t get_slider_value(PF_ParamDef* params[], const FilmSimulationCtrl idx) noexcept
+{
+    return static_cast<int32_t>(params[UnderlyingType(idx)]->u.sd.value);
+}
+
+inline const double get_float_slider_value(PF_ParamDef* params[], const FilmSimulationCtrl idx) noexcept
+{
+    return params[UnderlyingType(idx)]->u.fs_d.value;
+}
+
+inline constexpr bool is_control_available(const film::eFILM_CONTROL_BIT bit, const uint64_t mask) noexcept
+{
+    // The entire calculation is done inside a single return statement
+    return static_cast<bool>((1ull << UnderlyingType(bit)) & mask);
+}
+
--- AFTEREFFECT_PLUGIN.DEV/FilmSimulation/src/AlgoControlAdobe.cpp
+++ AFTEREFFECT_PLUGIN.DEV/FilmSimulation/src/AlgoControlAdobe.cpp
@@ -1,19 +1,41 @@
-#include "Common.hpp"
-#include "CompileTimeUtils.hpp"
+#include "AE_Effect.h"
 #include "AlgoControl.hpp"
+#include "AlgoControlAdobe.hpp"
 #include "AlgoControlEnums.hpp"
 #include "AlgoAdobeControlEnums.hpp"
 #include "film_params_mask.hpp"
-#include "AE_Effect.h"
+
 
 
 AlgoControls getAlgoControls (PF_ParamDef* params[], const double fps)
 {
     CACHE_ALIGN AlgoControls algoParams = getAlgoControlsDefault();
 
-    algoParams.filmProfile = static_cast<film::eFILM_PROFILE>(params[UnderlyingType(FilmSimulationCtrl::FILM_STOCK)]->u.pd.value - 1);
+    algoParams.filmProfile = get_list_box_value<film::eFILM_PROFILE>(params, FilmSimulationCtrl::FILM_STOCK);
     algoParams.frameRate   = ((fps < 1) ? 24.0 : fps);
-//    algoParams.filmFormat  = static_cast<FilmFormatCtrl>(params[UnderlyingType(FilmSimulationCtrl::FILM_FORMAT)]->u.pd.value - 1);
+
+    const uint64_t filmMask = film::kFilmControlAvailability[UnderlyingType(algoParams.filmProfile)];
+
+    if (is_control_available(film::eCTRL_BIT_FILM_FORMAT, filmMask))
+        algoParams.filmFormat  = get_list_box_value<FilmFormatCtrl>(params, FilmSimulationCtrl::FILM_FORMAT);
+    if (is_control_available(film::eCTRL_BIT_PROCESS_VARIANT, filmMask))
+        algoParams.processVariant = get_list_box_value<ProcessVariantCtrl>(params, FilmSimulationCtrl::PROCESS_VARIANT);
+    if (is_control_available(film::eCTRL_BIT_EXPOSURE_STOPS, filmMask))
+        algoParams.exposureStops = get_float_slider_value(params, FilmSimulationCtrl::EXPOSURE);
+    if (is_control_available(film::eCTRL_BIT_EXPOSURE_TIME_S, filmMask))
+        algoParams.exposureTimeS = get_float_slider_value(params, FilmSimulationCtrl::EXPOSURE_TIME);
+    if (is_control_available(film::eCTRL_BIT_GREY_TARGET, filmMask))
+        algoParams.greyTarget = get_float_slider_value(params, FilmSimulationCtrl::MID_GRAY_TARGET);
+    if (is_control_available(film::eCTRL_BIT_BLACK_POINT_STRETCH, filmMask))
+        algoParams.blackPointStretch = get_float_slider_value(params, FilmSimulationCtrl::BLACK_POINT_STRETCH);
+    if (is_control_available(film::eCTRL_BIT_DEVELOPMENT_MINUTES, filmMask))
+        algoParams.developmentMinutes = get_float_slider_value(params, FilmSimulationCtrl::DEVELOPMENT_TIME);
+    if (is_control_available(film::eCTRL_BIT_DEVELOPMENT_CELSIUS, filmMask))
+        algoParams.developmentCelsius = get_float_slider_value(params, FilmSimulationCtrl::DEVELOPMENT_TEMPERATURE);
+    if (is_control_available(film::eCTRL_BIT_STORAGE_YEARS, filmMask))
+        algoParams.storageYears = get_slider_value(params, FilmSimulationCtrl::YEARS_OF_DARK_STORAGE);
+    if (is_control_available(film::eCTRL_BIT_SCENE_KELVIN, filmMask))
+        algoParams.sceneKelvin = get_slider_value(params, FilmSimulationCtrl::SCENE_COLOUR_TEMPERATURE);
 
     return algoParams;
 }
